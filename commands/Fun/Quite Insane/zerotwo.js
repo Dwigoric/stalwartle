e9870043b70e1f795d2f497dde5852953a53f5db
Dwigoric <dwigoric@gmail.com>
2018-06-15 08:33:52 +0800

    Add `zerotwo` command; update to 2.1.1c

---
+++ commands/Fun/Quite Insane/zerotwo.js
@@ -0,0 +1,22 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			runIn: ['text'],
+			requiredPermissions: ['ATTACH_FILES'],
+			description: "Make Zero Two love your/someone else's picture!",
+			extendedHelp: [
+				'By default, if you do not provide a user, your avatar will be used.',
+				'To have a different person, use `s.zerotwo @Mention`'
+			].join('\n'),
+			usage: '[Love:user]'
+		});
+	}
+
+	async run(msg, [love = msg.author]) {
+		msg.channel.sendFile(await this.client.idiot.zerotwo(love.displayAvatarURL({ format: 'png' })), 'zerotwo.png');
+	}
+
+};
