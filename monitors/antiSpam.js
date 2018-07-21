ebe0670a7c648e2ad1e93e36268df6cb4e56fd99
Dwigoric <dwigoric@gmail.com>
2018-07-22 06:52:04 +0800

    Run finalizer after calling commands manually

--- monitors/antiSpam.js
+++ monitors/antiSpam.js
@@ -19,9 +19,16 @@ module.exports = class extends Monitor {
 		if (msg.member.messages.length <= 5) return null;
 		if (msg.channel.postable) msg.channel.send(`Hey ${msg.author}! No spamming allowed, or I'll punish you!`);
 		if (msg.channel.permissionsFor(this.client.user).has('MANAGE_MESSAGES')) msg.member.messages.forEach(message => message.delete().catch(() => null));
-		return this.client.commands.get('mute')
-			.run(msg, [msg.member, await this.client.arguments.get('time').run('3m', '', msg), 'Spamming with AntiSpam enabled'], true)
+
+		const duration = await this.client.arguments.get('time').run('3m', '', msg);
+		this.client.commands.get('mute')
+			.run(msg, [msg.member, duration, 'Spamming with AntiSpam enabled'], true)
 			.catch(err => msg.send(err));
+		this.client.finalizers.get('modlogging').run({
+			command: this.client.commands.get('mute'),
+			channel: msg.channel,
+			guild: msg.guild
+		}, [msg.author, 'Spamming with AntiSpam enabled', duration]);
 	}
 
 	async init() {
