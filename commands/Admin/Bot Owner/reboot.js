e2c6090a170aca335e480147253ad9d020be25b5
Dwigoric <dwigoric@gmail.com>
2018-08-10 21:49:17 +0800

    Use native `destroy()` method instead of exiting process directly

--- commands/Admin/Bot Owner/reboot.js
+++ commands/Admin/Bot Owner/reboot.js
@@ -13,7 +13,7 @@ module.exports = class extends Command {
 	async run(msg) {
 		await this.client.configs.update('restart', msg.channel.id);
 		await msg.send(`<a:loading:430269209415516160>  ::  Bot is restarting...`);
-		process.exit();
+		this.client.destroy();
 	}
 
 };
