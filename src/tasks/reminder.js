d66f1ce1b03f1a781a1d367e8f00de063e874285
Dwigoric <dwigoric@gmail.com>
2018-10-10 21:48:35 +0800

    Fix the reminder task on uncached users

--- src/tasks/reminder.js
+++ src/tasks/reminder.js
@@ -4,7 +4,7 @@ module.exports = class extends Task {
 
 	async run({ channel, user, text, forceChannel }) {
 		const _channel = this.client.channels.get(channel);
-		const _user = this.client.users.get(user);
+		const _user = await this.client.users.fetch(user);
 		const _text = text ? `me to remind you: ${text}` : 'to be reminded!';
 		const reminder = forceChannel ? `Hey there, people of ${_channel}! You wanted ${_text}` : `Hey there, ${_user}! You wanted ${_text}`;
 		if (forceChannel) return _channel.send(reminder).catch(() => null);
