2563de9f8bb13acf4a8c2f8f591df7d403095e32
Dwigoric <dwigoric@gmail.com>
2018-10-28 22:31:23 +0800

    Fix autoMute

--- src/extendables/actions.js
+++ src/extendables/actions.js
@@ -18,4 +18,8 @@ module.exports = class Actions extends Extendable {
 		this.client.setTimeout(() => Actions[this.id].shift(), 300000);
 	}
 
+	async resetActions() {
+		Actions[this.id] = [];
+	}
+
 };
