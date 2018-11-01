435767b4959c1834389dd3c77dd42ad0c5e1775a
Dwigoric <dwigoric@gmail.com>
2018-11-01 08:49:18 +0800

    Undo commit 7266a0a

--- src/commands/General/Chat Bot Info/help.js
+++ src/commands/General/Chat Bot Info/help.js
@@ -33,7 +33,6 @@ module.exports = class extends Command {
 		}
 
 		const method = this.client.user.bot ? 'author' : 'channel';
-		if (!category.toTitleCase) throw `<:redTick:399433440975519754>  ::  **${category}** is not a valid command! Only use the name of the command.`;
 		const help = await this.buildHelp(msg, [category ? category.toTitleCase() : undefined, subcategory ? subcategory.toTitleCase() : undefined]);
 		const categories = Object.keys(help);
 		const helpMessage = [];
