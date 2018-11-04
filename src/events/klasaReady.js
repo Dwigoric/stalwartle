32dc918fffea8aed708ab8b8791856b8fb76ce4a
Dwigoric <dwigoric@gmail.com>
2018-11-04 18:44:10 +0800

    Change status via `setInterval`

--- src/events/klasaReady.js
+++ src/events/klasaReady.js
@@ -1,42 +1,39 @@
 const { Event } = require('klasa');
 const { MessageEmbed } = require('discord.js');
 
+const statuses = [
+	{ name: 'dead', type: 'PLAYING' },
+	{ name: 'with your feelings', type: 'PLAYING' },
+	{ name: 'with sparkling 🔥', type: 'PLAYING' },
+	{ name: 'hide and seek', type: 'PLAYING' },
+	{ name: 'bad code', type: 'LISTENING' },
+	{ name: 'with magic', type: 'PLAYING' },
+	{ name: 'Cops and Robbers', type: 'PLAYING' },
+	{ name: 'Simon Says', type: 'PLAYING' },
+	{ name: 'I Spy', type: 'PLAYING' },
+	{ name: 'chess', type: 'PLAYING' },
+	{ name: 'with a rubber duck', type: 'PLAYING' },
+	{ name: 'your movements', type: 'WATCHING' },
+	{ name: 'Stranger Things', type: 'WATCHING' },
+	{ name: 'Steven Universe', type: 'WATCHING' },
+	{ name: 'anime', type: 'WATCHING' },
+	{ name: 'Spotify', type: 'LISTENING' },
+	{ name: 'Pop Rock', type: 'LISTENING' },
+	{ name: 'P!ATD', type: 'LISTENING' },
+	{ name: 'Fall Out Boy', type: 'LISTENING' },
+	{ name: 'Ariana Grande', type: 'LISTENING' },
+	{ name: 'Little Mix', type: 'LISTENING' }
+];
+
 module.exports = class extends Event {
 
 	async run() {
 		this.client.setGuildCount();
 		this.client.user.setActivity('Just started running! 👀', { type: 'WATCHING' }).then(() => {
-			const statusLoop = () => {
-				setTimeout(() => {
-					const statuses = [
-						{ name: 'dead', type: 'PLAYING' },
-						{ name: 'with your feelings', type: 'PLAYING' },
-						{ name: 'with sparkling 🔥', type: 'PLAYING' },
-						{ name: 'hide and seek', type: 'PLAYING' },
-						{ name: 'bad code', type: 'LISTENING' },
-						{ name: 'with magic', type: 'PLAYING' },
-						{ name: 'Cops and Robbers', type: 'PLAYING' },
-						{ name: 'Simon Says', type: 'PLAYING' },
-						{ name: 'I Spy', type: 'PLAYING' },
-						{ name: 'chess', type: 'PLAYING' },
-						{ name: 'with a rubber duck', type: 'PLAYING' },
-						{ name: 'your movements', type: 'WATCHING' },
-						{ name: 'Stranger Things', type: 'WATCHING' },
-						{ name: 'Steven Universe', type: 'WATCHING' },
-						{ name: 'anime', type: 'WATCHING' },
-						{ name: 'Spotify', type: 'LISTENING' },
-						{ name: 'Pop Rock', type: 'LISTENING' },
-						{ name: 'P!ATD', type: 'LISTENING' },
-						{ name: 'Fall Out Boy', type: 'LISTENING' },
-						{ name: 'Ariana Grande', type: 'LISTENING' },
-						{ name: 'Little Mix', type: 'LISTENING' }
-					];
-					const status = statuses[Math.floor(Math.random() * statuses.length)];
-					this.client.user.setActivity(`${status.name} | ${this.client.options.prefix}help`, { type: status.type });
-					statusLoop();
-				}, 60000);
-			};
-			setTimeout(() => statusLoop(), 10000);
+			setInterval(() => {
+				const status = statuses[Math.floor(Math.random() * statuses.length)];
+				this.client.user.setActivity(`${status.name} | ${this.client.options.prefix}help`, { type: status.type });
+			}, 60000);
 		});
 		const { restart } = this.client.settings;
 		if (!restart) return;
