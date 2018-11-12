f70ec7f85aa6fa14d84203bb039495f966fded4d
Dwigoric <dwigoric@gmail.com>
2018-11-12 20:20:56 +0800

    Fix mentionSpam module on emitting the event

--- src/monitors/mentionSpam.js
+++ src/monitors/mentionSpam.js
@@ -18,15 +18,19 @@ module.exports = class extends Monitor {
 		if (msg.author.equals(this.client.user)) return null;
 
 		if (msg.member.messages.length && msg.member.messages
-			.map(message => message.mentions.users ? message.mentions.users.size : 0 + message.mentions.bots ? message.mentions.bots.size : 0)
+			.map(message => message.mentions.users ? message.mentions.users.size : 0 + message.mentions.roles ? message.mentions.roles.size : 0)
 			.reduce((prev, val) => prev + val) < 10) return null;
 		if (msg.channel.postable) msg.channel.send(`Hey ${msg.author}! Don't spam mentions, ${msg.author}. Got it, ${msg.author}?`);
 		if (!msg.member.bannable) {
-			return this.client.finalizers.get('modlogging').run({
+			return this.client.emit('modlogAction', {
 				command: this.client.commands.get('warn'),
 				channel: msg.channel,
-				guild: msg.guild
-			}, [msg.author, 'Spamming mentions with the MentionSpam enabled (member has higher permissions so I could not ban them)', null, msg.content.length > 900 ? null : msg.content]);
+				guild: msg.guild,
+				content: msg.content.length > 900 ? [
+					`**Roles**: ${msg.member.messages.map(message => message.mentions.roles.map(rl => rl.toString()).join(', ')).join(', ')}`,
+					`**Users**: ${msg.member.messages.map(message => message.mentions.users.map(us => us.toString()).join(', ')).join(', ')}`
+				].join('\n') : msg.content
+			}, msg.author, 'Spamming mentions with the MentionSpam enabled (member has higher permissions so I could not ban them)', null);
 		} // eslint-disable-line max-len
 		if (msg.channel.permissionsFor(this.client.user).has('MANAGE_MESSAGES')) msg.member.messages.forEach(message => message.delete().catch(() => null));
 		return this.client.commands.get('ban')
