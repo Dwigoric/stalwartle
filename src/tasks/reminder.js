279581f6d6cdd1dc9d7664b385cccb020f99a2d3
Dwigoric <dwigoric@gmail.com>
2018-11-14 20:26:31 +0800

    Fix not found channels on reminder task

--- src/tasks/reminder.js
+++ src/tasks/reminder.js
@@ -7,6 +7,7 @@ module.exports = class extends Task {
 		const _user = await this.client.users.fetch(user);
 		const _text = text ? `me to remind you: ${text}` : 'to be reminded!';
 		const reminder = forceChannel ? `Hey there, people of ${_channel}! You wanted ${_text}` : `Hey there, ${_user}! You wanted ${_text}`;
+		if (!_channel) return null;
 		if (forceChannel) return _channel.send(reminder).catch(() => null);
 		else return _user.send(reminder).catch(() => _channel.send(reminder).catch(() => null));
 	}
