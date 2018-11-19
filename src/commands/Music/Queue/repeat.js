86f5f10cd3b0b710293a5b4c4a51c604d86b1560
Dwigoric <dwigoric@gmail.com>
2018-11-19 23:56:05 +0800

    Enable showing the current repeat status

--- src/commands/Music/Queue/repeat.js
+++ src/commands/Music/Queue/repeat.js
@@ -7,13 +7,19 @@ module.exports = class extends Command {
 			aliases: ['loop'],
 			permissionLevel: 5,
 			description: 'Sets if the current song will be repeated or the whole queue.',
-			usage: '<song|queue|none>'
+			usage: '[song|queue|none]'
 		});
+		this.symbols = {
+			song: '🔂',
+			queue: '🔁',
+			none: '➡'
+		};
 	}
 
 	async run(msg, [repeat]) {
+		if (!repeat) return msg.send(`${this.symbols[msg.guild.settings.get('music.repeat')]}  ::  Music sessions in this server are set to repeat ${msg.guild.settings.get('music.repeat') === 'none' ? 'nothing' : `the ${msg.guild.settings.get('music.repeat')}`}.`); // eslint-disable-line max-len
 		msg.guild.settings.update('music.repeat', repeat);
-		msg.send(`<:check:508594899117932544>  ::  Music sessions in this server are now set to repeat ${repeat === 'none' ? 'nothing' : `the ${repeat}`}.`);
+		return msg.send(`${this.symbols[repeat]}  ::  Music sessions in this server are now set to repeat ${repeat === 'none' ? 'nothing' : `the ${repeat}`}.`);
 	}
 
 };
