45e0c52b6961d70c63cb8a3f1107c72750dc3487
Dwigoric <dwigoric@gmail.com>
2018-11-19 22:59:27 +0800

    Escape markdown on currently playing on queue

--- src/commands/Music/Queue/queue.js
+++ src/commands/Music/Queue/queue.js
@@ -52,7 +52,7 @@ module.exports = class extends Command {
 			const info = await ytdl.getBasicInfo(music);
 			return `\`${currentPos}\`. **${escapeMarkdown(info.title)}** by ${escapeMarkdown(info.author.name)}`;
 		})))).then(songs => songs.forEach(songList => display.addPage(template => template.setDescription(
-			[`${npStatus} **${np.title}** by ${np.author.name}\n`].concat(songList).join('\n')
+			[`${npStatus} **${escapeMarkdown(np.title)}** by ${escapeMarkdown(np.author.name)}\n`].concat(songList).join('\n')
 		))));
 
 		return display
