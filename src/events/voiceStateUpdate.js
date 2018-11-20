efe9c797b11f730b3eb3a74562baed2f7d9fed05
Dwigoric <dwigoric@gmail.com>
2018-11-20 15:32:15 +0800

    Fix misspelling for voiceStateUpdate

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -3,7 +3,7 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run(oldState, newState) {
-		if (!newState.guild.voiceConnection || !newState.guild.voicecConnection.dispatcher) return;
+		if (!newState.guild.voiceConnection || !newState.guild.voiceConnection.dispatcher) return;
 		if (newState.guild.me.voice.channel.members.filter(mb => mb.id !== newState.guild.me.id).size) return;
 		this.client.setTimeout(voiceConnection => {
 			if (voiceConnection.channel.members.filter(mb => !mb.user.bot).size) return;
