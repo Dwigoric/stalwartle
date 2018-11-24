a64af8673aba5f6dc4d090057de76fd1ff5e0042
Dwigoric <dwigoric@gmail.com>
2018-11-24 09:36:36 +0800

    Fix removal of current song in clear command

--- src/commands/Music/Queue/clear.js
+++ src/commands/Music/Queue/clear.js
@@ -11,7 +11,7 @@ module.exports = class extends Command {
 	}
 
 	async run(msg) {
-		this.client.providers.default.update('music', msg.guild.id, { queue: [] });
+		this.client.providers.default.update('music', msg.guild.id, { queue: await this.client.providers.default.get('music', msg.guild.id).then(ms => ms.queue.splice(0, 1)) });
 		msg.send('<:check:508594899117932544>  ::  Successfully cleared the music queue for this server.');
 	}
 
