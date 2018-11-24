be5d92e3262a72a9308fe5c984860e984cf88bd5
Dwigoric <dwigoric@gmail.com>
2018-11-24 10:37:49 +0800

    Fix seeking on times longer than the song's

--- src/commands/Music/Control/seek.js
+++ src/commands/Music/Control/seek.js
@@ -16,7 +16,7 @@ module.exports = class extends Command {
 		seek -= Date.now();
 		const { queue } = await this.client.providers.default.get('music', msg.guild.id);
 		if (!queue.length || !msg.guild.player.channel || !msg.guild.player.playing) throw `<:error:508595005481549846>  ::  No song playing! Add one using \`${msg.guildSettings.get('prefix')}play\``; // eslint-disable-line max-len
-		if (parseInt(queue[0].info.length < seek)) throw '<:error:508595005481549846>  ::  The time you supplied is longer than the song\'s length.'; // eslint-disable-line max-len
+		if (queue[0].info.length < seek) throw '<:error:508595005481549846>  ::  The time you supplied is longer than the song\'s length.';
 		msg.guild.player.seek(seek);
 		return msg.send('<:check:508594899117932544>  ::  Successfully seeked the music.');
 	}
