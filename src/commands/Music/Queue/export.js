f4b25c03ae1f5df7b498a83c3482dc83330d063e
Dwigoric <dwigoric@gmail.com>
2018-11-24 21:36:29 +0800

    Add export command

---
+++ src/commands/Music/Queue/export.js
@@ -0,0 +1,37 @@
+const { Command } = require('klasa');
+const fetch = require('node-fetch');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			runIn: ['text'],
+			description: 'Exports the server music queue.'
+		});
+	}
+
+	async run(msg) {
+		const { queue } = await this.client.providers.default.get('music', msg.guild.id);
+		if (!queue.length) throw `<:error:508595005481549846>  ::  The queue is empty. Add one using the \`${msg.guildSettings.get('prefix')}play\` command.`;
+		let choice;
+		do {
+			choice = await msg.prompt('📜  ::  Should the queue be exported to `haste`/`hastebin` or `file`? Please reply with your respective answer.').catch(() => ({ content: 'none' }));
+		} while (!['file', 'haste', 'hastebin', 'none', null].includes(choice.content));
+		switch (choice.content) {
+			case 'file': {
+				if (!msg.channel.attachable) throw '<:error:508595005481549846>  ::  I do not have the permissions to attach files to this channel.';
+				return msg.channel.sendFile(Buffer.from(queue.map((track, position) => `${position}  ::  ${track.info.uri}`).join('\r\n')), 'output.txt', 'Sent the result as file.');
+			}
+			case 'haste':
+			case 'hastebin': {
+				const { key } = await fetch('https://hastebin.com/documents', {
+					method: 'POST',
+					body: queue.map((track, position) => `${position}  ::  ${track.info.uri}`).join('\r\n')
+				}).then(res => res.json());
+				return msg.send(`Sent the result to hastebin: <https://hastebin.com/${key}>`);
+			}
+		}
+		return null;
+	}
+
+};
