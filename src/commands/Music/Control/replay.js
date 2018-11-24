3f10ad970e35d03a56ac9f3a5ea9d66202b4beb2
Dwigoric <dwigoric@gmail.com>
2018-11-24 12:57:26 +0800

    Remove unnecessary comment in replay.js

--- src/commands/Music/Control/replay.js
+++ src/commands/Music/Control/replay.js
@@ -11,7 +11,7 @@ module.exports = class extends Command {
 	}
 
 	async run(msg) {
-		if (!msg.guild.player.channel || !msg.guild.player.playing) throw `<:error:508595005481549846>  ::  No song playing! Add one using \`${msg.guildSettings.get('prefix')}play\``; // eslint-disable-line max-len
+		if (!msg.guild.player.channel || !msg.guild.player.playing) throw `<:error:508595005481549846>  ::  No song playing! Add one using \`${msg.guildSettings.get('prefix')}play\``;
 		msg.guild.player.seek(0);
 		return msg.send('<:check:508594899117932544>  ::  Successfully replayed the music.');
 	}
