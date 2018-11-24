36af259f679470715061b29cb0a984439592658d
Dwigoric <dwigoric@gmail.com>
2018-11-24 10:28:19 +0800

    Remove playing restriction in voiceStateUpdate

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -3,7 +3,7 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run(oldState, newState) {
-		if (!newState.guild.player.channel || !newState.guild.player.playing) return;
+		if (!newState.guild.player.channel) return;
 		if (newState.guild.channels.get(newState.guild.player.channel).members.filter(mb => !mb.user.bot).size) return;
 		this.client.setTimeout(guild => {
 			if (guild.channels.get(guild.player.channel).members.filter(mb => !mb.user.bot).size) return;
