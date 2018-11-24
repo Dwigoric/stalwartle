66b7a8b9c597b55f54cbb717db7651b66f7038ab
Dwigoric <dwigoric@gmail.com>
2018-11-24 10:23:52 +0800

    Update voiceStateUpdate event (ref: 1539e57)

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -3,13 +3,12 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run(oldState, newState) {
-		if (!newState.guild.voiceConnection || !newState.guild.voiceConnection.dispatcher) return;
-		if (newState.guild.me.voice.channel.members.filter(mb => mb.id !== newState.guild.me.id).size) return;
-		this.client.setTimeout(voiceConnection => {
-			if (voiceConnection.channel.members.filter(mb => !mb.user.bot).size) return;
-			voiceConnection.dispatcher.destroy();
-			voiceConnection.channel.leave();
-		}, 30000, newState.guild.voiceConnection);
+		if (!newState.guild.player.channel || !newState.guild.player.playing) return;
+		if (newState.guild.channels.get(newState.guild.player.channel).members.filter(mb => !mb.user.bot).size) return;
+		this.client.setTimeout(guild => {
+			if (guild.channels.get(guild.player.channel).members.filter(mb => !mb.user.bot).size) return;
+			this.client.player.leave(guild.id);
+		}, 30000, newState.guild);
 	}
 
 };
