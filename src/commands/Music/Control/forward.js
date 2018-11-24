ee5398d9ca6ae44db97b05697f22595a0a08796d
Dwigoric <dwigoric@gmail.com>
2018-11-24 12:59:27 +0800

    Add forward and rewind commands

---
+++ src/commands/Music/Control/forward.js
@@ -0,0 +1,22 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			runIn: ['text'],
+			permissionLevel: 5,
+			description: 'Forwards the current song by the specified time.',
+			extendedHelp: 'To use this command use e.g. `22m 29s` to forward the song by 22 minutes and 29 seconds.',
+			usage: '<SeekTime:time>'
+		});
+	}
+
+	async run(msg, [seek]) {
+		seek -= Date.now();
+		if (!msg.guild.player.channel || !msg.guild.player.playing) throw `<:error:508595005481549846>  ::  No song playing! Add one using \`${msg.guildSettings.get('prefix')}play\``;
+		msg.guild.player.seek(msg.guild.player.state.position + seek);
+		return msg.send('<:check:508594899117932544>  ::  Successfully forwarded the music.');
+	}
+
+};
