274c406bf349d1dc251546185ecebe52ba004909
Dwigoric <dwigoric@gmail.com>
2018-11-25 22:56:03 +0800

    Fix logic in musicControl inhibitor

--- src/inhibitors/musicControl.js
+++ src/inhibitors/musicControl.js
@@ -5,7 +5,7 @@ module.exports = class extends Inhibitor {
 	async run(msg, command) {
 		if (!msg.guild) return;
 		if (!this.client.commands.filter(cmd => cmd.category === 'Music' && cmd.subCategory === 'Control').has(command.name)) return;
-		if (!await msg.hasAtLeastPermissionLevel(5)) return;
+		if (await msg.hasAtLeastPermissionLevel(5)) return;
 		if (!msg.guild.player.channel) return;
 		const chan = msg.guild.channels.get(msg.guild.player.channel);
 		if (!chan.members.has(msg.member.id)) throw `<:error:508595005481549846>  ::  You must be connected to #**${chan.name}** to be able to control the music session.`;
