fc0925f76f829fb829a9c5f9c5c6c6821c24cf3f
Dwigoric <dwigoric@gmail.com>
2018-11-25 17:06:03 +0800

    *Actually* ignore music unrelated activity

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -5,7 +5,9 @@ module.exports = class extends Event {
 	run(oldState, newState) {
 		if (!this.client.player) return null;
 		if (!newState.guild.player.channel) return null;
-		if (oldState.channel && oldState.channel.id !== oldState.guild.player.channel && newState.channel && newState.channel.id !== newState.guild.player.channel) return null;
+		if (newState.channel && newState.channel.id !== newState.guild.player.channel) return null;
+		if (oldState.channel && oldState.channel.id !== newState.guild.player.channel) return null;
+		if (oldState.channel && newState.channel && oldState.channel.id === newState.channel.id) return null;
 		if (newState.guild.channels.get(newState.guild.player.channel).members.filter(mb => !mb.user.bot).size) return newState.guild.player.pause(false);
 		newState.guild.player.pause(true);
 		return this.client.setTimeout(guild => {
