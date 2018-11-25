cd21a112750d9b7c2e5fd219dd1918496c9c383b
Dwigoric <dwigoric@gmail.com>
2018-11-25 12:25:55 +0800

    Resume player if a user reconnects within 30 seconds

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -3,13 +3,13 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run(oldState, newState) {
-		if (!this.client.player) return;
-		if (!newState.guild.player.channel) return;
-		if (newState.guild.channels.get(newState.guild.player.channel).members.filter(mb => !mb.user.bot).size) return;
+		if (!this.client.player) return null;
+		if (!newState.guild.player.channel) return null;
+		if (newState.guild.channels.get(newState.guild.player.channel).members.filter(mb => !mb.user.bot).size) return newState.guild.player.pause(false);
 		newState.guild.player.pause(true);
-		this.client.setTimeout(guild => {
-			if (guild.player.channel && guild.channels.get(guild.player.channel).members.filter(mb => !mb.user.bot).size) return;
-			this.client.player.leave(guild.id);
+		return this.client.setTimeout(guild => {
+			if (guild.player.channel && guild.channels.get(guild.player.channel).members.filter(mb => !mb.user.bot).size) return null;
+			return this.client.player.leave(guild.id);
 		}, 30000, newState.guild);
 	}
 
