71ec9605b9ad2fac1b7aa07860e0e3baefba1928
Dwigoric <dwigoric@gmail.com>
2018-11-25 12:23:22 +0800

    Pause player if no user is connected

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -6,6 +6,7 @@ module.exports = class extends Event {
 		if (!this.client.player) return;
 		if (!newState.guild.player.channel) return;
 		if (newState.guild.channels.get(newState.guild.player.channel).members.filter(mb => !mb.user.bot).size) return;
+		newState.guild.player.pause(true);
 		this.client.setTimeout(guild => {
 			if (guild.player.channel && guild.channels.get(guild.player.channel).members.filter(mb => !mb.user.bot).size) return;
 			this.client.player.leave(guild.id);
