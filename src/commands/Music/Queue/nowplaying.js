f615f275a33aaa90c3953d8bbdc629caddb5f4a3
Dwigoric <dwigoric@gmail.com>
2018-11-25 08:33:01 +0800

    Destructure nowplaying.js

--- src/commands/Music/Queue/nowplaying.js
+++ src/commands/Music/Queue/nowplaying.js
@@ -15,12 +15,12 @@ module.exports = class extends Command {
 	async run(msg) {
 		const { queue } = await this.client.providers.default.get('music', msg.guild.id);
 		if (!queue.length || !msg.guild.player.channel || !msg.guild.player.playing) throw '<:error:508595005481549846>  ::  There is no music playing in this server!';
-		const npDuration = parseInt(queue[0].info.length);
-		const playedDuration = parseInt(msg.guild.player.state.position);
-		const timestamp = new Timestamp(npDuration >= 3600000 ? 'hh:mm:ss' : 'mm:ss');
+		const { length } = queue[0].info;
+		const { position } = msg.guild.player.state;
+		const timestamp = new Timestamp(length >= 3600000 ? 'hh:mm:ss' : 'mm:ss');
 
 		const progress = '▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬'.split('');
-		progress.splice(Math.ceil(((playedDuration / npDuration) || 0.01) * progress.length) - 1, 1, '🔘');
+		progress.splice(Math.ceil(((position / length) || 0.01) * progress.length) - 1, 1, '🔘');
 
 		return msg.send({
 			embed: new MessageEmbed()
@@ -29,9 +29,9 @@ module.exports = class extends Command {
 				.setColor('RANDOM')
 				.setAuthor(`🎶 Now Playing on ${msg.guild.name}`)
 				.setFooter(`by ${queue[0].info.author}`)
-				.setDescription(`${progress.join('')} ${queue[0].info.isStream ? 'N/A' : `${parseInt((playedDuration / npDuration) * 100)}%`}`)
+				.setDescription(`${progress.join('')} ${queue[0].info.isStream ? 'N/A' : `${parseInt((position / length) * 100)}%`}`)
 				.addField('Repeat', toTitleCase(msg.guild.settings.get('music.repeat')), true)
-				.addField('Time', queue[0].info.isStream ? 'N/A - YouTube Livestream' : `\`${timestamp.display(playedDuration)} / ${timestamp.display(npDuration)}\``, true)
+				.addField('Time', queue[0].info.isStream ? 'N/A - YouTube Livestream' : `\`${timestamp.display(position)} / ${timestamp.display(length)}\``, true)
 		});
 	}
 
