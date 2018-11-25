c958e5d6c4a02bf9b65a7c0421e6f0df7718db70
Dwigoric <dwigoric@gmail.com>
2018-11-25 11:51:00 +0800

    Forgot that @

--- src/commands/General/Chat Bot Info/prefix.js
+++ src/commands/General/Chat Bot Info/prefix.js
@@ -16,7 +16,7 @@ module.exports = class extends Command {
 		if (!newPrefix) throw `The prefix for this server is currently \`${prefix}\`. Please use \`${prefix}prefix <prefix>\` to change the server prefix.`;
 		if (!await msg.hasAtLeastPermissionLevel(6)) throw '<:error:508595005481549846>  ::  Sorry! Only moderators or people with Manage Server permission may change the server prefix.'; // eslint-disable-line max-len
 		msg.guild.settings.update('prefix', newPrefix);
-		msg.send(`<:check:508594899117932544>  ::  The prefix for **${msg.guild.name}** is now \`${newPrefix}\`. Type \`${this.client.user.tag}\` to get the current prefix.`);
+		msg.send(`<:check:508594899117932544>  ::  The prefix for **${msg.guild.name}** is now \`${newPrefix}\`. Type \`@${this.client.user.tag}\` to get the current prefix.`);
 	}
 
 };
