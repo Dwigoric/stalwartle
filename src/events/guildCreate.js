65edaa5026a7f486e7e9f3afc231204381e29287
Dwigoric <dwigoric@gmail.com>
2018-11-27 01:14:38 +0800

    Fix guildCreate and guildDelete

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -10,17 +10,16 @@ module.exports = class extends Event {
 
 	async run(guild) {
 		this.client.setGuildCount();
-		this.hook.send({
-			embed: new MessageEmbed()
-				.setColor(0x2ECC71)
-				.setAuthor("I've been added to a new server!", guild.owner.user.displayAvatarURL())
-				.setThumbnail(guild.iconURL({ format: 'png' }))
-				.setTitle(`${guild.name}  |  ${guild.id}`)
-				.addField('Guild Owner', `${guild.owner.user.tag} (${guild.owner.user})`)
-				.addField('Large Guild', guild.large ? '<:check:508594899117932544>' : '<:error:508595005481549846>')
-				.addField('New Guild Count', await this.client.guildCount(), true)
-				.setTimestamp()
-		});
+		this.hook.send(new MessageEmbed()
+			.setColor(0x2ECC71)
+			.setAuthor("I've been added to a new server!", guild.owner.user.displayAvatarURL())
+			.setThumbnail(guild.iconURL({ format: 'png' }))
+			.setTitle(`${guild.name}  |  ${guild.id}`)
+			.addField('Guild Owner', `${guild.owner.user.tag} (${guild.owner.user})`)
+			.addField('Large Guild', guild.large ? '<:check:508594899117932544>' : '<:error:508595005481549846>', true)
+			.addField('New Guild Count', await this.client.guildCount(), true)
+			.setTimestamp()
+		);
 	}
 
 	async init() {
--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -10,17 +10,16 @@ module.exports = class extends Event {
 
 	async run(guild) {
 		this.client.setGuildCount();
-		this.hook.send({
-			embed: new MessageEmbed()
-				.setColor(0xE74C3C)
-				.setAuthor("I've been removed from a server", guild.owner.user.displayAvatarURL())
-				.setThumbnail(guild.iconURL({ format: 'png' }))
-				.setTitle(`${guild.name}  |  ${guild.id}`)
-				.addField('Guild Owner', `${guild.owner.user.tag} (${guild.owner.user})`)
-				.addField('Large Guild', guild.large ? '<:check:508594899117932544>' : '<:error:508595005481549846>')
-				.addField('New Guild Count', await this.client.guildCount(), true)
-				.setTimestamp()
-		});
+		this.hook.send(new MessageEmbed()
+			.setColor(0xE74C3C)
+			.setAuthor("I've been removed from a server", guild.owner.user.displayAvatarURL())
+			.setThumbnail(guild.iconURL({ format: 'png' }))
+			.setTitle(`${guild.name}  |  ${guild.id}`)
+			.addField('Guild Owner', `${guild.owner.user.tag} (${guild.owner.user})`)
+			.addField('Large Guild', guild.large ? '<:check:508594899117932544>' : '<:error:508595005481549846>', true)
+			.addField('New Guild Count', await this.client.guildCount(), true)
+			.setTimestamp()
+		);
 	}
 
 	async init() {
