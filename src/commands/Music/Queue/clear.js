7e1fe653928c4da4c792474e691a155d127e3ad4
Dwigoric <dwigoric@gmail.com>
2018-11-27 22:59:30 +0800

    Enable clearing whole queue

--- src/commands/Music/Queue/clear.js
+++ src/commands/Music/Queue/clear.js
@@ -11,7 +11,7 @@ module.exports = class extends Command {
 	}
 
 	async run(msg) {
-		this.client.providers.default.update('music', msg.guild.id, { queue: await this.client.providers.default.get('music', msg.guild.id).then(ms => ms.queue.splice(0, 1)) });
+		this.client.providers.default.update('music', msg.guild.id, { queue: msg.guild.player.playing ? await this.client.providers.default.get('music', msg.guild.id).then(ms => ms.queue.slice(0, 1)) : [] }); // eslint-disable-line max-len
 		msg.send('<:check:508594899117932544>  ::  Successfully cleared the music queue for this server.');
 	}
 
