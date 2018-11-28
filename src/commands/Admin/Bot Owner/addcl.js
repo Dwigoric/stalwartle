b63943066f00710fd420206a0236bbb7a6e35c91
Dwigoric <dwigoric@gmail.com>
2018-11-28 20:27:27 +0800

    Use only one changelog channel in addcl

--- src/commands/Admin/Bot Owner/addcl.js
+++ src/commands/Admin/Bot Owner/addcl.js
@@ -14,17 +14,12 @@ module.exports = class extends Command {
 	}
 
 	async run(msg, ...params) {
-		[
-			// ChillGalaxy
-			'445823150626832386',
-			// Insanity Bot Laboratory
-			'401349122902458378'
-		].forEach(chan => this.client.channels.get(chan).send({
+		this.client.channels.get(this.client.settings.get('changelogs')).send({
 			embed: new MessageEmbed()
 				.setTitle(`<a:updating:417233654545383424> ${this.client.user.username}'s Changelog`)
 				.setDescription(params)
 				.setTimestamp()
-		}));
+		});
 		msg.send(`<:check:508594899117932544>  ::  Successfully posted changelog!`);
 	}
 
