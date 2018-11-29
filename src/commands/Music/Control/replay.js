55f1ee0f8bec578c51126be806772ccd4dee267f
Dwigoric <dwigoric@gmail.com>
2018-11-29 21:30:04 +0800

    Specify action (ref: 5973645)

--- src/commands/Music/Control/replay.js
+++ src/commands/Music/Control/replay.js
@@ -13,7 +13,7 @@ module.exports = class extends Command {
 	async run(msg) {
 		const song = await this.client.providers.default.get('music', msg.guild.id).then(ms => ms.queue[0]);
 		if (!msg.guild.player.channel || !msg.guild.player.playing) throw `<:error:508595005481549846>  ::  No song playing! Add one using \`${msg.guildSettings.get('prefix')}play\``;
-		if (!song.info.isSeekable) throw '<:error:508595005481549846>  ::  The current track playing is an online livestream.';
+		if (!song.info.isSeekable) throw '<:error:508595005481549846>  ::  The current track playing cannot be replayed.';
 		msg.guild.player.seek(0);
 		msg.guild.player.pause(false);
 		return msg.send('<:check:508594899117932544>  ::  Successfully replayed the music.');
--- src/commands/Music/Control/rewind.js
+++ src/commands/Music/Control/rewind.js
@@ -16,7 +16,7 @@ module.exports = class extends Command {
 		seek -= Date.now();
 		const song = await this.client.providers.default.get('music', msg.guild.id).then(ms => ms.queue[0]);
 		if (!msg.guild.player.channel || !msg.guild.player.playing) throw `<:error:508595005481549846>  ::  No song playing! Add one using \`${msg.guildSettings.get('prefix')}play\``;
-		if (!song.info.isSeekable) throw '<:error:508595005481549846>  ::  The current track playing is an online livestream.';
+		if (!song.info.isSeekable) throw '<:error:508595005481549846>  ::  The current track playing cannot be rewinded.';
 		msg.guild.player.seek(msg.guild.player.state.position - seek);
 		return msg.send('<:check:508594899117932544>  ::  Successfully rewinded the music.');
 	}
