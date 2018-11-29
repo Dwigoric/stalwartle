597364500bdd6d073de7f77bcda9f814f9a73652
Dwigoric <dwigoric@gmail.com>
2018-11-29 21:27:16 +0800

    Inform user on seeking livestreams

--- src/commands/Music/Control/forward.js
+++ src/commands/Music/Control/forward.js
@@ -14,7 +14,9 @@ module.exports = class extends Command {
 
 	async run(msg, [seek]) {
 		seek -= Date.now();
+		const song = await this.client.providers.default.get('music', msg.guild.id).then(ms => ms.queue[0]);
 		if (!msg.guild.player.channel || !msg.guild.player.playing) throw `<:error:508595005481549846>  ::  No song playing! Add one using \`${msg.guildSettings.get('prefix')}play\``;
+		if (!song.info.isSeekable) throw '<:error:508595005481549846>  ::  The current track playing is an online livestream.';
 		msg.guild.player.seek(msg.guild.player.state.position + seek);
 		return msg.send('<:check:508594899117932544>  ::  Successfully forwarded the music.');
 	}
