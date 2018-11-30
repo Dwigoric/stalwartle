4f8ba455d91183cd33f2eb1dba70433790fa08d0
Dwigoric <dwigoric@gmail.com>
2018-11-30 19:32:46 +0800

    Add option to set gain for all bands

--- src/commands/Music/Control/equalize.js
+++ src/commands/Music/Control/equalize.js
@@ -11,16 +11,26 @@ module.exports = class extends Command {
 				'There are 15 bands (`0` to `14`) that can be changed.',
 				'**Gain** is the multiplier for the given band. The default value of gain each band is `0`.',
 				'Valid values range from `-0.25` to `1.0`, where `-0.25` means the given band is completely muted, and `0.25` means it is doubled.',
-				'\nModifying the gain could also change the volume of the output.'
+				'\nModifying the gain could also change the volume of the output.',
+				'\nTo set gain for all bands, replace the `band` with `setall`.'
 			].join('\n'),
-			usage: '<Band:integer{0,14}> <Gain:number{-0.25,1.0}>',
+			usage: '<setall|Band:integer{0,14}> <Gain:number{-0.25,1.0}>',
 			usageDelim: ' '
 		});
 	}
 
 	async run(msg, [band, gain]) {
+		if (band === 'setall') {
+			const bands = [];
+			for (band of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]) bands.push({ band, gain });
+			msg.guild.player.setEQ(bands);
+			return msg.send(`<:check:508594899117932544>  ::  Successfully equalized all bands' gain to \`${gain}\`.`);
+		}
 		msg.guild.player.setEQ([{ gain, band }]);
-		return msg.send(`<:check:508594899117932544>  ::  Successfully equalized band \`#${band}\`'s gain to \`${gain}\`.`);
+		return msg.send([
+			`<:check:508594899117932544>  ::  Successfully equalized band \`#${band}\`'s gain to \`${gain}\`.`,
+			`*Make sure you know what you're doing. Run \`${msg.guild.settings.get('prefix')}eq setall 0\` if you're unsure.*`
+		].join(' '));
 	}
 
 };
