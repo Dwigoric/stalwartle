46ce9fba865d8f9daf9cb9fcd7ad8c33e6447b02
Dwigoric <dwigoric@gmail.com>
2018-11-30 18:54:14 +0800

    *Had* to do it

--- src/commands/Music/Control/equalize.js
+++ src/commands/Music/Control/equalize.js
@@ -7,13 +7,13 @@ module.exports = class extends Command {
 			runIn: ['text'],
 			aliases: ['eq'],
 			description: 'Equalizes the song player using bands and gains.',
-			usage: '<Band:integer{0,14}> <Gain:number{-0.25,1.0}>',
 			extendedHelp: [
 				'There are 15 bands (`0` to `14`) that can be changed.',
 				'**Gain** is the multiplier for the given band. The default value of gain each band is `0`.',
 				'Valid values range from `-0.25` to `1.0`, where `-0.25` means the given band is completely muted, and `0.25` means it is doubled.',
 				'\nModifying the gain could also change the volume of the output.'
 			].join('\n'),
+			usage: '<Band:integer{0,14}> <Gain:number{-0.25,1.0}>',
 			usageDelim: ' '
 		});
 	}
