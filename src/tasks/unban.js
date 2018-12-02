4732c2bbe847871eaf38bd8a9947e6d5dae57f0c
Dwigoric <dwigoric@gmail.com>
2018-12-02 20:36:23 +0800

    Fix automated unban

--- src/tasks/unban.js
+++ src/tasks/unban.js
@@ -6,10 +6,10 @@ module.exports = class extends Task {
 		const _guild = this.client.guilds.get(guild);
 		if (!await _guild.fetchBans().then(bans => bans.has(user))) return null;
 		const _user = await this.client.users.fetch(user).catch(() => null);
-		this.client.finalizers.get('modlogging').run({
+		this.client.emit('modlogging', {
 			command: this.client.commands.get('unban'),
 			guild: _guild
-		}, [_user, 'Auto Unban']);
+		}, _user, 'Auto Unban');
 		return _guild.members.unban(_user, 'Auto Unban');
 	}
 
