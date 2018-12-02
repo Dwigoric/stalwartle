9fde645580cf016d9089298020497bebb8a19bc3
Dwigoric <dwigoric@gmail.com>
2018-12-02 14:55:21 +0800

    Send message when bot is invited

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -21,6 +21,23 @@ module.exports = class extends Event {
 			.addField('New Guild Count', await this.client.guildCount(), true)
 			.setTimestamp()
 		);
+
+		const message = new MessageEmbed()
+			.setColor('#C62A29')
+			.setAuthor('Thank you for having me!', guild.owner.user.displayAvatarURL())
+			.setTitle(guild.name)
+			.setFooter(`${this.client.user.username} Added!`, this.client.user.displayAvatarURL())
+			.setThumbnail(guild.iconURL({ size: 2048 }))
+			.setTimestamp()
+			.setDescription([
+				`Hey there ${guild.owner}! Thank you for having me in **${guild.name}**. It is an honor to serve you.`,
+				`To begin using me, please use \`${guild.settings.get('prefix')}help\` here or on any text channel. You will be given a list of commands.`,
+				`Please feel free to look at the command list. If you want me to serve more Discord users, just use the \`${guild.settings.get('prefix')}invite\` command!`,
+				'\nI can play music, moderate users, send memes, manipulate user avatars, and more!'
+			].join('\n'));
+		const postableChannel = guild.channels.filter(ch => ch.type === 'text' && ch.postable).first();
+		if (!postableChannel) return guild.owner.user.sendEmbed(message, guild.owner).catch(() => null);
+		return postableChannel.sendEmbed(message, guild.owner);
 	}
 
 	async init() {
