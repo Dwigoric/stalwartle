d846f48d199e3d066ffce5c04ef2a3fab537e2bb
Dwigoric <dwigoric@gmail.com>
2018-12-09 19:08:13 +0800

    Show custom prefix on mute and unmute

--- src/commands/Moderation/Action/unmute.js
+++ src/commands/Moderation/Action/unmute.js
@@ -14,7 +14,7 @@ module.exports = class extends Command {
 	}
 
 	async run(msg, [member, ...reason]) {
-		if (!msg.guild.settings.get('muteRole')) throw '<:error:508595005481549846>  ::  The mute role has not yet been set up for this server. You can do so by using the `s.muterole` command.';
+		if (!msg.guild.settings.get('muteRole')) throw `<:error:508595005481549846>  ::  The mute role has not yet been set up for this server. You can do so by using the \`${msg.guild.settings.get('prefix')}muterole\` command.`; // eslint-disable-line max-len
 		if (member.user.equals(msg.author)) throw 'Look... how are you able to use this command if you were already unmuted?';
 		if (member.user.equals(this.client.user)) throw '*Unmutes self*. Oh, I would not have been able to respond if I were in the first place!';
 
@@ -26,7 +26,7 @@ module.exports = class extends Command {
 
 		reason = reason.length ? reason.join(this.usageDelim) : null;
 		const muteRole = msg.guild.roles.get(msg.guild.settings.get('muteRole'));
-		if (!muteRole) throw '<:error:508595005481549846>  ::  Whoops! The mute role has been deleted. Please reconfigure this server\'s mute role by using the `s.muterole` command.';
+		if (!muteRole) throw `<:error:508595005481549846>  ::  Whoops! The mute role has been deleted. Please reconfigure this server's mute role by using the \`${msg.guild.settings.get('prefix')}muterole\` command.`; // eslint-disable-line max-len
 		if (!member.roles.has(muteRole.id)) throw `<:error:508595005481549846>  ::  ${user.tag} wasn't muted already!`;
 
 		await member.roles.remove(muteRole, 'Unmuted');
