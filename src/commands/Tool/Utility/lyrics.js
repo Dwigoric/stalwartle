eafb8685fcf9acd06e8a265b6b541db98ae317d3
Dwigoric <dwigoric@gmail.com>
2018-12-14 20:31:57 +0800

    Use ksoft API on lyrics

--- src/commands/Tool/Utility/lyrics.js
+++ src/commands/Tool/Utility/lyrics.js
@@ -1,6 +1,5 @@
 const { Command } = require('klasa');
 const fetch = require('node-fetch');
-const cheerio = require('cheerio');
 
 module.exports = class extends Command {
 
@@ -12,17 +11,13 @@ module.exports = class extends Command {
 	}
 
 	async run(msg, [query]) {
-		const { hits } = await fetch(`https://api.genius.com/search?access_token=${this.client.auth.geniusAPIkey}&q=${encodeURIComponent(query)}`)
-			.then(res => res.json())
-			.then(body => body.response);
-		if (!hits.length) throw '<:error:508595005481549846>  ::  No song lyrics found.';
-		const $c = await cheerio.load(await fetch(hits[0].result.url).then(res => res.text()));
-		const lyrics = $c('.lyrics').text().trim().split('\n');
+		const { data } = await fetch(`https://api.ksoft.si/lyrics/search?q=${encodeURIComponent(query)}`, { headers: { Authorization: `Bearer ${this.client.auth.ksoftAPIkey}` } }).then(res => res.json());
+		if (!data.length) throw '<:error:508595005481549846>  ::  No song lyrics found.';
+		const lyrics = data[0].lyrics.split('\n');
 		while (lyrics.indexOf('') >= 0) lyrics.splice(lyrics.indexOf(''), 1, '\u200b');
 		const fullLyrics = [
-			`__**${hits[0].result.full_title}**__\n`,
-			lyrics.join('\n'),
-			'\n__*Powered by Genius (https://genius.com)*__'
+			`__***${data[0].name}** by ${data[0].artist}*${data[0].album ? ` on ${data[0].album}` : ''}__`,
+			lyrics.join('\n')
 		].join('\n');
 
 		const swearArray = (msg.guild ? msg.guild.settings.get('automod.swearWords').map(word => `(?:^|\\W)${word}(?:$|\\W)`) : []).concat([
@@ -44,7 +39,6 @@ module.exports = class extends Command {
 		]).map(word => `(?:^|\\W)${word}(?:$|\\W)`);
 		const swearRegex = new RegExp(swearArray.join('|'), 'im');
 		if (swearRegex.test(fullLyrics) && msg.guild && !msg.channel.nsfw) throw '<:error:508595005481549846>  ::  The song contains NSFW lyrics and this channel is not marked as NSFW.';
-		if (lyrics.join('\n').length > 10000) throw '<:error:508595005481549846>  ::  Whoops! The result does not seem to be a song... Please try another search query.';
 
 		return msg.channel.send(fullLyrics, { split: { char: '\u200b' } }).catch(() => msg.channel.send(fullLyrics, { split: true }));
 	}
