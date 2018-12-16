690a6b9feecfa38cffad236a0eaf64e5ae17c6c4
Dwigoric <dwigoric@gmail.com>
2018-12-16 20:06:07 +0800

    Fix sending message to owner on autorole

--- src/events/autoroleAdd.js
+++ src/events/autoroleAdd.js
@@ -9,7 +9,7 @@ module.exports = class extends Event {
 	async run(member) {
 		const autorole = member.guild.settings.get('autorole');
 		if (!autorole.bot && !autorole.user) return null;
-		if (!member.guild.me.permissions.has('MANAGE_ROLES')) return member.guild.owner.send(`⚠ I don't have **Manage Roles** permission on ${member.guild.name}, so I couldn't give ${member.user.tag} the autorole.`); // eslint-disable-line max-len
+		if (!member.guild.me.permissions.has('MANAGE_ROLES')) return member.guild.owner.user.send(`⚠ I don't have **Manage Roles** permission on ${member.guild.name}, so I couldn't give ${member.user.tag} the autorole.`); // eslint-disable-line max-len
 		if (member.user.bot && autorole.bot) return await this.giveRole(member, 'bot');
 		if (!member.user.bot && autorole.user) return await this.giveRole(member, 'user');
 		return null;
@@ -18,7 +18,7 @@ module.exports = class extends Event {
 	async giveRole(member, type) {
 		const role = member.guild.roles.get(member.guild.settings.get(`autorole.${type}`));
 		if (!role) {
-			member.guild.owner.send(`The role **${member.guild.settings.get(`autorole.${type}`)}** doesn't exist anymore. Autorole aborted.`).catch(() => null);
+			member.guild.owner.user.send(`The role **${member.guild.settings.get(`autorole.${type}`)}** doesn't exist anymore. Autorole aborted.`).catch(() => null);
 			return member.guild.settings.reset(`autorole.${type}`);
 		}
 		if (role.position >= member.guild.me.roles.highest.position) return member.guild.owner.user.send(`⚠ **${role.name}**'s position was higher than my highest role, therefore I couldn't assign that autorole to anyone.`).catch(() => null); // eslint-disable-line max-len
