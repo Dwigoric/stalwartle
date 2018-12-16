9b5dfe75dc18c2c7c0e6a491697281b96978916d
Dwigoric <dwigoric@gmail.com>
2018-12-16 10:57:38 +0800

    Update lyrics command

--- src/commands/Tool/Utility/lyrics.js
+++ src/commands/Tool/Utility/lyrics.js
@@ -13,11 +13,10 @@ module.exports = class extends Command {
 	async run(msg, [query]) {
 		const { data } = await fetch(`https://api.ksoft.si/lyrics/search?q=${encodeURIComponent(query)}`, { headers: { Authorization: `Bearer ${this.client.auth.ksoftAPIkey}` } }).then(res => res.json());
 		if (!data.length) throw '<:error:508595005481549846>  ::  No song lyrics found.';
-		const lyrics = data[0].lyrics.split('\n');
-		while (lyrics.indexOf('') >= 0) lyrics.splice(lyrics.indexOf(''), 1, '\u200b');
 		const fullLyrics = [
 			`__***${data[0].name}** by ${data[0].artist}*${data[0].album ? ` on ${data[0].album}` : ''}__`,
-			lyrics.join('\n')
+			data[0].lyrics,
+			'\n__*Powered by KSoft.Si API*__ '
 		].join('\n');
 
 		const swearArray = (msg.guild ? msg.guild.settings.get('automod.swearWords').map(word => `(?:^|\\W)${word}(?:$|\\W)`) : []).concat([
