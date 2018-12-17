0d88a886f6da8e4b22a5be96c8bf0d00bf6f674a
Dwigoric <dwigoric@gmail.com>
2018-12-17 13:55:43 +0800

    Add region and ?verified info on guildCreate/Delete

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,6 +1,16 @@
 const { Event } = require('klasa');
 const { WebhookClient, MessageEmbed, Util: { escapeMarkdown } } = require('discord.js');
 
+const gregion = {
+	'eu-central': 'Central Europe',
+	'eu-west': 'Western Europe',
+	hongkong: 'Hong Kong',
+	'us-west': 'Western US',
+	'us-east': 'Eastern US',
+	'us-south': 'Southern US',
+	'us-central': 'Central US'
+};
+
 module.exports = class extends Event {
 
 	constructor(...args) {
@@ -10,6 +20,13 @@ module.exports = class extends Event {
 
 	async run(guild) {
 		this.client.setGuildCount();
+
+		const regionArr = guild.region.split('-');
+		if (regionArr.includes('vip')) regionArr.splice(regionArr.indexOf('vip'), 1);
+		const rawRegion = regionArr.join('-');
+		let region = gregion[rawRegion] || rawRegion.replace(/^./, i => i.toUpperCase());
+		if (guild.region.includes('vip')) region += ' [Partnered]';
+
 		this.hook.send(new MessageEmbed()
 			.setColor(0x2ECC71)
 			.setAuthor("I've been added to a new server!", guild.owner.user.displayAvatarURL())
@@ -17,8 +34,10 @@ module.exports = class extends Event {
 			.setTitle(`${escapeMarkdown(guild.name)}  |  ${guild.id}`)
 			.addField('Guild Owner', `${guild.owner.user.tag} (${guild.owner.user})`)
 			.addField('Large Guild', guild.large ? '✅' : '❌', true)
+			.addField('Verified Guild', guild.verified ? '✅' : '❌', true)
 			.addField('Guild Members', guild.memberCount, true)
 			.addField('New Guild Count', await this.client.guildCount(), true)
+			.addField('Guild Region', region, true)
 			.setTimestamp()
 		);
 	}
