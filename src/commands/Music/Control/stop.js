e124d58ae898cfa70edcaa8ca372af2cd71ec20a
Dwigoric <dwigoric@gmail.com>
2018-12-18 23:49:27 +0800

    Clarify that `stop` command does not clear the queue

--- src/commands/Music/Control/stop.js
+++ src/commands/Music/Control/stop.js
@@ -13,7 +13,7 @@ module.exports = class extends Command {
 	async run(msg) {
 		if (!msg.guild.player.channel) throw '<:error:508595005481549846>  ::  There is no music session in this server.';
 		this.client.player.leave(msg.guild.id);
-		return msg.send('<:check:508594899117932544>  ::  Successfully ended the music session for this server.');
+		return msg.send(`<:check:508594899117932544>  ::  Successfully ended the music session for this server. Use \`${msg.guild.settings.get('prefix')}clear\` to clear the queue.`);
 	}
 
 };
