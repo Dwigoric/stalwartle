555ff025d25d54fd7cd0190c5f779bf08b566a38
Dwigoric <dwigoric@gmail.com>
2018-12-18 23:41:58 +0800

    Clarify give & take if didn't pass permcheck

--- src/commands/Moderation/Server Management/take.js
+++ src/commands/Moderation/Server Management/take.js
@@ -15,7 +15,7 @@ module.exports = class extends Command {
 
 	async run(msg, [member, role]) {
 		if (!role) throw `<:error:508595005481549846>  ::  Whoops! I think **${role}** doesn't exist... Maybe use the role's ID instead?`;
-		if (role.position >= msg.guild.me.roles.highest.position) throw `<:error:508595005481549846>  ::  ${role.name} is higher than or equal to me!`;
+		if (role.position >= msg.guild.me.roles.highest.position) throw `<:error:508595005481549846>  ::  ${role.name} has higher or equal position to my highest role!`;
 		if (!member.roles.has(role.id)) throw `<:error:508595005481549846>  ::  ${member} already doesn't have **${role.name}**! I mean, what's the point of taking something from someone they already don't have?`; // eslint-disable-line max-len
 		await member.roles.remove(role, `Taken using ${this.client.user.username}'s Take Role feature`);
 		return msg.send(`<:check:508594899117932544>  ::  Successfully taken the role **${role.name}** from ${member}.`);
