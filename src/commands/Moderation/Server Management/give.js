808cc4e4e4ac7d2752ce6961cf1a02b1b64e9e07
Dwigoric <dwigoric@gmail.com>
2018-12-18 23:31:48 +0800

    Fix give and take permchecks

--- src/commands/Moderation/Server Management/give.js
+++ src/commands/Moderation/Server Management/give.js
@@ -15,8 +15,8 @@ module.exports = class extends Command {
 
 	async run(msg, [member, role]) {
 		if (!role) throw `<:error:508595005481549846>  ::  Whoops! I think **${role}** doesn't exist... Maybe use the role's ID instead?`;
-		if (member.roles.highest.position >= msg.guild.me.roles.highest.position) throw `<:error:508595005481549846>  ::  Sorry! I can't edit ${member}'s roles.`;
-		if (member.roles.highest.position >= msg.member.roles.highest.position) throw `<:error:508595005481549846>  ::  It seems you cannot edit ${member}'s roles...`;
+		if (member.permissions.bitfield >= msg.guild.me.permissions.bitfield) throw `<:error:508595005481549846>  ::  Sorry! I can't edit ${member}'s roles.`;
+		if (member.permissions.bitfield >= msg.member.permissions.bitfield) throw `<:error:508595005481549846>  ::  It seems you cannot edit ${member}'s roles...`;
 		if (member.roles.has(role.id)) throw `<:error:508595005481549846>  ::  ${member} already has **${role.name}**! I mean, what's the point of giving someone something they already have?`; // eslint-disable-line max-len
 		await member.roles.add(role, `Given using ${this.client.user.username}'s Give Role feature`);
 		return msg.send(`<:check:508594899117932544>  ::  Successfully given ${member} the role **${role.name}**.`);
