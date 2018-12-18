850333ef511a1c539702f8068f809303a39266bc
Dwigoric <dwigoric@gmail.com>
2018-12-18 22:13:10 +0800

    Change way of cleaning music history

--- src/tasks/shiftHistory.js
+++ src/tasks/shiftHistory.js
@@ -2,10 +2,25 @@ const { Task } = require('klasa');
 
 module.exports = class extends Task {
 
-	async run({ guild }) {
-		const { queue, playlist, history } = await this.client.providers.default.get('music', guild);
-		history.shift();
-		this.client.providers.default.update('music', guild, { queue, playlist, history });
+	async run() {
+		for (const { id } of await this.client.providers.default.getAll('music')) {
+			const { queue, playlist, history } = await this.client.providers.default.get('music', id);
+			this.client.providers.default.update('music', id, {
+				queue,
+				playlist,
+				history: history.filter(hist => (Date.now() - hist.timestamp) <= (1000 * 60 * 60 * 24))
+			});
+		}
+
+		// Create a schedule to make this task work
+		while (this.client.schedule.tasks.filter(tk => tk.taskName === 'shiftHistory').length !== 1) {
+			this.client.schedule.tasks.filter(tk => tk.taskName === 'shiftHistory').forEach(tk => this.client.schedule.delete(tk.id));
+			await this.client.schedule.create('shiftHistory', '*/7 * * * *');
+		}
+	}
+
+	async init() {
+		this.run();
 	}
 
 };
