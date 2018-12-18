2b8f09956f40aec4d7b86c381e72d29340e33ad0
Dwigoric <dwigoric@gmail.com>
2018-12-19 01:00:21 +0800

    Enable adding cases

---
+++ src/commands/Moderation/Server Management/addcase.js
@@ -0,0 +1,59 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Adds cases to modlog in case of a manual action.',
+			usage: '<kick|ban|unban|softban|mute|unmute> <Target:user> [Duration:duration] <Reason:string> [...]',
+			usageDelim: ' ',
+			subcommands: true
+		});
+	}
+
+	async kick(msg, [target, , ...reason]) {
+		reason = reason.join(this.usageDelim);
+		if (!(await msg.guild.fetchAuditLogs()).entries.filter(entry => entry.action === 'MEMBER_KICK' && entry.executor === msg.author.id && entry.target.id === target.id).size) throw `<:error:508595005481549846>  ::  The server audit logs show that there are no instances that you kicked **${target.tag}**.`; // eslint-disable-line max-len
+		msg.command = this.client.commands.get('kick');
+		this.client.emit('modlogAction', msg, target, reason);
+	}
+
+	async ban(msg, [target, duration, ...reason]) {
+		reason = reason.join(this.usageDelim);
+		if (!(await msg.guild.fetchAuditLogs()).entries.filter(entry => entry.action === 'MEMBER_BAN' && entry.executor === msg.author.id && entry.target.id === target.id).size) throw `<:error:508595005481549846>  ::  The server audit logs show that there are no instances that you banned **${target.tag}**.`; // eslint-disable-line max-len
+		msg.command = this.client.commands.get('ban');
+		this.client.emit('modlogAction', msg, target, reason, duration);
+	}
+
+	async unban(msg, [target, , ...reason]) {
+		reason = reason.join(this.usageDelim);
+		if (!(await msg.guild.fetchAuditLogs()).entries.filter(entry => entry.action === 'MEMBER_UNBAN' && entry.executor === msg.author.id && entry.target.id === target.id).size) throw `<:error:508595005481549846>  ::  The server audit logs show that there are no instances that you unbanned **${target.tag}**.`; // eslint-disable-line max-len
+		msg.command = this.client.commands.get('unban');
+		this.client.emit('modlogAction', msg, target, reason);
+	}
+
+	async softban(msg, [target, , ...reason]) {
+		reason = reason.join(this.usageDelim);
+		if (!(await msg.guild.fetchAuditLogs()).entries.filter(entry => entry.action === 'MEMBER_BAN' && entry.executor === msg.author.id && entry.target.id === target.id).size) throw `<:error:508595005481549846>  ::  The server audit logs show that there are no instances that you banned **${target.tag}**.`; // eslint-disable-line max-len
+		if (!(await msg.guild.fetchAuditLogs()).entries.filter(entry => entry.action === 'MEMBER_UNBAN' && entry.executor === msg.author.id && entry.target.id === target.id).size) throw `<:error:508595005481549846>  ::  The server audit logs show that there are no instances that you unbanned **${target.tag}**.`; // eslint-disable-line max-len
+		msg.command = this.client.commands.get('softban');
+		this.client.emit('modlogAction', msg, target, reason);
+	}
+
+	async mute(msg, [target, duration, ...reason]) {
+		reason = reason.join(this.usageDelim);
+		if (!msg.guild.settings.get('muteRole')) throw `<:error:508595005481549846>  ::  The mute role has not yet been set up for this server. You can do so by using the \`${msg.guild.settings.get('prefix')}muterole\` command.`; // eslint-disable-line max-len
+		if (!(await msg.guild.fetchAuditLogs()).entries.filter(entry => entry.action === 'MEMBER_ROLE_UPDATE' && entry.executor === msg.author.id && entry.target.id === target.id && entry.changes[0].key === '$add').size) throw `<:error:508595005481549846>  ::  The server audit logs show that there are no instances that you gave the muterole to **${target.tag}**.`; // eslint-disable-line max-len
+		msg.command = this.client.commands.get('mute');
+		this.client.emit('modlogAction', msg, target, reason, duration);
+	}
+
+	async unmute(msg, [target, , ...reason]) {
+		reason = reason.join(this.usageDelim);
+		if (!msg.guild.settings.get('muteRole')) throw `<:error:508595005481549846>  ::  The mute role has not yet been set up for this server. You can do so by using the \`${msg.guild.settings.get('prefix')}muterole\` command.`; // eslint-disable-line max-len
+		if (!(await msg.guild.fetchAuditLogs()).entries.filter(entry => entry.action === 'MEMBER_ROLE_UPDATE' && entry.executor === msg.author.id && entry.target.id === target.id && entry.changes[0].key === '$remove').size) throw `<:error:508595005481549846>  ::  The server audit logs show that there are no instances that you gave the muterole to **${target.tag}**.`; // eslint-disable-line max-len
+		msg.command = this.client.commands.get('unmute');
+		this.client.emit('modlogAction', msg, target, reason);
+	}
+
+};
