ea15d54baf49594b29c1f2f15c4839e45f57c8fa
Dwigoric <dwigoric@gmail.com>
2018-12-26 13:28:19 +0800

    Await firstTime inhibitor to finish

--- src/inhibitors/firstTime.js
+++ src/inhibitors/firstTime.js
@@ -4,20 +4,20 @@ module.exports = class extends Inhibitor {
 
 	async run(msg, command) {
 		if (!msg.guild) return null;
-		if (command.category === 'Moderation') return this.checkTable(msg.guild.id, 'modlogs');
-		if (command.category === 'Music') return this.checkTable(msg.guild.id, 'music');
+		if (command.category === 'Moderation') return await this.checkTable(msg.guild.id, 'modlogs');
+		if (command.category === 'Music') return await this.checkTable(msg.guild.id, 'music');
 		return null;
 	}
 
 	async checkTable(guild, table) {
 		const defProvider = this.client.providers.default;
-		if (await this.client.providers.default.get(table, guild)) return;
+		if (await this.client.providers.default.get(table, guild)) return null;
 		await defProvider.create(table, guild);
 		const obj = {
 			modlogs: { modlogs: [] },
 			music: { history: [], playlist: [], queue: [] }
 		};
-		await defProvider.update(table, guild, obj[table]);
+		return await defProvider.update(table, guild, obj[table]);
 	}
 
 };
