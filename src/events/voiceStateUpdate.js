f6b25d2fe9b5eb248219bc7127dd408118397b8d
Dwigoric <dwigoric@gmail.com>
2019-02-03 13:34:53 +0800

    Pause music only on non-stream tracks regardless of guild donation

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -2,15 +2,16 @@ const { Event } = require('klasa');
 
 module.exports = class extends Event {
 
-	run(oldState, newState) {
+	async run(oldState, newState) {
 		if (!this.client.player) return null;
-		if (newState.guild.settings.get('donation') >= 8) return null;
 		if (!newState.guild.player.channel) return null;
 		if (newState.channel && newState.channel.id !== newState.guild.player.channel) return null;
 		if (oldState.channel && oldState.channel.id !== newState.guild.player.channel) return null;
 		if (oldState.channel && newState.channel && oldState.channel.id === newState.channel.id) return null;
 		if (newState.guild.player.channel && newState.guild.channels.get(newState.guild.player.channel).members.filter(mb => !mb.user.bot).size) return newState.guild.player.pause(false);
-		newState.guild.player.pause(true);
+		const { queue } = await this.client.providers.default.get('music', newState.guild.id);
+		if (!queue[0].info.isStream) newState.guild.player.pause(true);
+		if (newState.guild.settings.get('donation') >= 8) return null;
 		return this.client.setTimeout(guild => {
 			if (guild.player.channel && guild.channels.get(guild.player.channel).members.filter(mb => !mb.user.bot).size) return null;
 			return this.client.player.leave(guild.id);
