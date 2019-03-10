4f5cd454a3ef90fe41f5fb31841ee11071e27f6f
Dwigoric <dwigoric@gmail.com>
2019-03-10 08:57:29 +0800

    Add fallback for empty votes in FML stories

--- src/commands/Fun/Insane/fml.js
+++ src/commands/Fun/Insane/fml.js
@@ -21,8 +21,8 @@ module.exports = class extends Command {
 			.setColor('RANDOM')
 			.setTimestamp()
 			.setDescription(`_${$('.block a').eq(0).text().trim()}\n\n_`)
-			.addField('I agree, your life sucks', $('.vote-up').eq(0).text(), true)
-			.addField('You deserved it:', $('.vote-down').eq(0).text(), true);
+			.addField('I agree, your life sucks', $('.vote-up').eq(0).text() || 'N/A', true)
+			.addField('You deserved it:', $('.vote-down').eq(0).text() || 'N/A', true);
 
 		if ($('.block a').length < 5) {
 			throw '<:akcry:333597917342466048>  ::  Today, something went wrong, so you will have to try again in a few moments. FML again.';
