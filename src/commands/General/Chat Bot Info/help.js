7cc032d37256d8dca80a77b95cfb67bcac909385
Dwigoric <dwigoric@gmail.com>
2019-03-25 10:08:24 +0800

    Update support server invite

--- src/commands/General/Chat Bot Info/help.js
+++ src/commands/General/Chat Bot Info/help.js
@@ -46,7 +46,7 @@ module.exports = class extends Command {
 						`\nWant to add ${this.client.user.username} to your own server or to a server you manage? If you have **Manage Server** permissions, you can add this bot by using the link:`,
 						`<http://bit.ly/${this.client.user.username.split(' ').join('-')}>`,
 						'\nNeed help or has ideas for the bot? Just want somewhere to hang out? Be with us here:',
-						`**${this.client.guilds.get('502895390807293963').name}** (<https://discord.gg/EH7jKFH>)`,
+						`**${this.client.guilds.get('502895390807293963').name}** (<https://discord.gg/KDWGvV8>)`,
 						`\nUse the command \`${this.client.options.prefix}bug\` to report a bug and \`${this.client.options.prefix}suggest\` if you have suggestions.`,
 						'\n__**DONATION PERKS**__',
 						'$3 ⇒ Enable the history and playlist features.',
