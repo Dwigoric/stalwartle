cb02520cfdf3303a13c73daed71fcd518f738feb
Dwigoric <dwigoric@gmail.com>
2019-04-04 21:27:31 +0800

    Interchange $8 and $10 donation perks

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -11,7 +11,7 @@ module.exports = class extends Event {
 		if (newState.guild.player.channel && newState.guild.channels.get(newState.guild.player.channel).members.filter(mb => !mb.user.bot).size) return newState.guild.player.pause(false);
 		const { queue } = await this.client.providers.default.get('music', newState.guild.id);
 		if (!queue[0].info.isStream) newState.guild.player.pause(true);
-		if (newState.guild.settings.get('donation') >= 8) return null;
+		if (newState.guild.settings.get('donation') >= 10) return null;
 		return this.client.setTimeout(guild => {
 			if (guild.player.channel && guild.channels.get(guild.player.channel).members.filter(mb => !mb.user.bot).size) return null;
 			return this.client.player.leave(guild.id);
