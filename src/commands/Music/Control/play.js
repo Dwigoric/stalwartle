172a2b331a5ce141454cdf6bbc4646879ac67670
Dwigoric <dwigoric@gmail.com>
2019-04-05 11:01:09 +0800

    Use random related video, not always first video

--- src/commands/Music/Control/play.js
+++ src/commands/Music/Control/play.js
@@ -170,7 +170,8 @@ module.exports = class extends Command {
 		guild.player.once('end', async data => {
 			if (data.reason === 'REPLACED') return null;
 			const { queue } = await this.client.providers.default.get('music', guild.id);
-			const relatedVideo = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${queue[0].info.identifier}&type=video&key=${this.client.auth.googleAPIkey}`).then(res => res.json()).then(res => res.items[0]).catch(() => null); // eslint-disable-line max-len
+			const results = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${queue[0].info.identifier}&type=video&key=${this.client.auth.googleAPIkey}`).then(res => res.json());
+			const relatedVideo = results.items[Math.floor(Math.random() * results.length)];
 			if (guild.settings.get('music.repeat') === 'queue') queue.push(queue[0]);
 			else if (guild.settings.get('music.repeat') !== 'song') queue.shift();
 			if (guild.settings.get('donation') >= 8 && guild.settings.get('music.autoplay') && !queue.length && Boolean(relatedVideo)) queue.push(mergeObjects((await this.getSongs(`https://youtu.be/${relatedVideo.id.videoId}`, false)).tracks[0], { requester: this.client.user.id, incognito: true })); // eslint-disable-line max-len
