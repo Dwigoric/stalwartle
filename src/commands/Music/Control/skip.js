c44b2b67eeebc476c58c82ef217a684560524aee
Dwigoric <dwigoric@gmail.com>
2019-04-25 15:06:43 +0800

    Fix skipping songs

--- src/commands/Music/Control/skip.js
+++ src/commands/Music/Control/skip.js
@@ -23,7 +23,7 @@ module.exports = class extends Command {
 			return msg.send('<:check:508594899117932544>  ::  Successfully forcibly skipped the music for this server.');
 		}
 		if (msg.guild.voteskips.includes(msg.author.id)) throw '<:error:508595005481549846>  ::  You\'ve already voted to skip the current song.';
-		const { members } = msg.guild.channels.get(msg.guild.player.channel);
+		const { members } = await this.client.channels.fetch(msg.guild.player.channel);
 		if (!members.has(msg.author.id)) throw '<:error:508595005481549846>  ::  You are not connected to the voice channel I\'m playing on.';
 		msg.guild.addVoteskip(msg.author.id, members);
 		const requiredVotes = members.filter(mb => !mb.user.bot).size / 2;
