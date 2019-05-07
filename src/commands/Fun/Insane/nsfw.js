83c8430d2c1176b9d0dc8d5cd2b9d91ef791b1d7
Dwigoric <dwigoric@gmail.com>
2019-05-07 11:01:22 +0800

    Add NSFW command

---
+++ src/commands/Fun/Insane/nsfw.js
@@ -0,0 +1,22 @@
+const { Command } = require('klasa');
+const fetch = require('node-fetch');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			nsfw: true,
+			requiredPermissions: ['ATTACH_FILES'],
+			description: 'Gives a random NSFW image. Must be in a NSFW channel.',
+			extendedHelp: 'If you use the flag `--gif`, it will give a GIF instead of a static image.'
+		});
+	}
+
+	async run(msg) {
+		const message = await msg.send('<a:loading:430269209415516160>  ::  Loading image...');
+		const result = await fetch(`https://api.ksoft.si/images/random-nsfw?gifs=${Boolean(msg.flags.gif)}`, { headers: { Authorization: `Bearer ${this.client.auth.ksoftAPIkey}` } }).then(res => res.json()); // eslint-disable-line max-len
+		await msg.channel.sendFile(result.image_url);
+		message.delete();
+	}
+
+};
