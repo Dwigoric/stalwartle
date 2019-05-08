b6a44aa6b0a426fba8453c4731a32d2e84efde14
Dwigoric <dwigoric@gmail.com>
2019-05-08 22:10:14 +0800

    Refine conditional statement for musicControl

--- src/inhibitors/musicControl.js
+++ src/inhibitors/musicControl.js
@@ -4,7 +4,7 @@ module.exports = class extends Inhibitor {
 
 	async run(msg, command) {
 		if (!msg.guild) return;
-		if (command.category !== 'Music' && command.subCategory !== 'Control') return;
+		if (command.category !== 'Music' || (command.category === 'Music' && command.subCategory !== 'Control')) return;
 		if (!await msg.hasAtLeastPermissionLevel(5) && command.name !== 'skip') throw '<:error:508595005481549846>  ::  Only DJs with the set DJ role can control music sessions.';
 		if (!msg.guild.player.channel) return;
 		const chan = msg.guild.channels.get(msg.guild.player.channel);
