543c5798a190014fbe7b6ce6e9521886abd613f3
Dwigoric <dwigoric@gmail.com>
2019-06-22 06:28:10 +0800

    Fix unprovided Steam genres

--- src/commands/Tool/Utility/steam.js
+++ src/commands/Tool/Utility/steam.js
@@ -40,7 +40,7 @@ module.exports = class extends Command {
 			['windows', 'mac', 'linux'].forEach(platform => {
 				if (steamData.platforms[platform]) platforms.push(platformsObj[platform]);
 			});
-			steamData.genres.forEach(genre => genres.push(genre.description));
+			if (steamData.genres) steamData.genres.forEach(genre => genres.push(genre.description));
 
 			embed
 				.setColor('RANDOM')
@@ -57,7 +57,7 @@ module.exports = class extends Command {
 				.addField(`Available Platform${platforms.length === 1 ? '' : 's'}`, platforms.join(', '), true)
 				.addField('Controller Support', steamData.controller_support ? toTitleCase(steamData.controller_support) : 'None', true)
 				.addField('Age Limit', steamData.required_age !== 0 ? steamData.required_age : 'Everyone', true)
-				.addField(`Genre${genres.length === 1 ? '' : 's'}`, genres.join(', '))
+				.addField(`Genre${genres.length === 1 ? '' : 's'}`, genres.length ? genres.join(', ') : 'N/A')
 				.addField(`Developer${steamData.developers.length === 1 ? '' : 's'}`, steamData.developers.join('\n').length ? steamData.developers : 'N/A', true)
 				.addField(`Publisher${steamData.publishers.length === 1 ? '' : 's'}`, steamData.publishers.join('\n').length ? steamData.publishers : 'N/A', true);
 			if (steamData.release_date.date) embed.addField('Date Released', steamData.release_date.date, true);
