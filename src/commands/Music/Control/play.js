5caa8eadad0fdf10aab0226fb4d52f05b1187185
Dwigoric <dwigoric@gmail.com>
2019-06-25 09:21:16 +0800

    Restrict music announcements to postable channels

--- src/commands/Music/Control/play.js
+++ src/commands/Music/Control/play.js
@@ -189,7 +189,7 @@ module.exports = class extends Command {
 			this.client.providers.default.update('music', guild.id, { history });
 		}
 		const announceChannel = guild.channels.get(guild.settings.get('music.announceChannel')) || channel;
-		if (guild.settings.get('music.announceSongs')) announceChannel.send(`🎧  ::  Now Playing: **${escapeMarkdown(song.info.title)}** by ${escapeMarkdown(song.info.author)} (Requested by **${escapeMarkdown(await guild.members.fetch(song.requester).then(req => req.displayName).catch(() => this.client.users.fetch(song.requester).then(user => user.tag)))}**)`); // eslint-disable-line max-len
+		if (guild.settings.get('music.announceSongs') && announceChannel.postable) announceChannel.send(`🎧  ::  Now Playing: **${escapeMarkdown(song.info.title)}** by ${escapeMarkdown(song.info.author)} (Requested by **${escapeMarkdown(await guild.members.fetch(song.requester).then(req => req.displayName).catch(() => this.client.users.fetch(song.requester).then(user => user.tag)))}**)`); // eslint-disable-line max-len
 	}
 
 	async init() {
