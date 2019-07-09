b2c647ca4b0c7b91a389890b9803bd822e9a3645
Dwigoric <dwigoric@gmail.com>
2019-07-09 19:57:50 +0800

    Whoops

--- src/commands/General/Chat Bot Info/help.js
+++ src/commands/General/Chat Bot Info/help.js
@@ -88,7 +88,7 @@ module.exports = class extends Command {
 						'$5 ⇒ Removal of 5-hour limit for each track in music. ($3 perk is included)',
 						'$8 ⇒ Autoplay songs (add related videos when queue is empty). Only applicable for YouTube videos. ($3 and $5 perks are included)',
 						'$10 ⇒ Make bot not leave within 30 seconds when no one is connected to voice channel; unless the bot is rebooted. ($3, $5, and $8 perks are included)',
-						'*Please pay at <https://patreon.com/Dwigoric>.',
+						'*Please pay at <https://patreon.com/Dwigoric>.*',
 						`*AFTER donating, contact ${this.client.application.owner.tag} or go to my support server to avail of these perks.*`
 					].join('\n') : '',
 					`\nBot developed by **${this.client.application.owner.tag}**, from 🇵🇭 with ❤`,
