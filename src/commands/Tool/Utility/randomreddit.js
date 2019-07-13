dd751f6881eefde70608e60f7c72ae039523dfaa
Dwigoric <dwigoric@gmail.com>
2019-07-14 07:25:20 +0800

    Allow embeds for randomreddit command

--- src/commands/Tool/Utility/randomreddit.js
+++ src/commands/Tool/Utility/randomreddit.js
@@ -1,4 +1,5 @@
 const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
 
 module.exports = class extends Command {
@@ -8,7 +9,7 @@ module.exports = class extends Command {
 			description: 'Returns a random reddit post on a given subreddit.',
 			usage: '<Subreddit:str>'
 		});
-		this.errorMessage = "There was an error. Reddit may be down, or the subreddit doesn't exist.";
+		this.errorMessage = "<:error:508595005481549846>  ::  There was an error. Reddit may be down, or the subreddit doesn't exist.";
 	}
 
 	async run(msg, [subreddit]) {
@@ -21,9 +22,18 @@ module.exports = class extends Command {
 			})
 			.catch(() => { throw this.errorMessage; });
 
-		if (data.over_18 && !msg.channel.nsfw) throw "I can't post a NSFW image in this channel unless you mark it as NSFW!";
+		if (data.over_18 && !msg.channel.nsfw) throw '<:error:508595005481549846>  ::  The post contains NSFW content and this channel is not marked as NSFW.';
 
-		return msg.sendMessage(data.url);
+		if (!msg.channel.permissionsFor(this.client.user).has('EMBED_LINKS')) return msg.sendMessage(`***${data.title}***\n\n${data.url}`);
+
+		return msg.sendEmbed(new MessageEmbed()
+			.setColor('RANDOM')
+			.setAuthor(`u/${data.author}`, null, `https://www.reddit.com/u/${data.author}`)
+			.setTitle(data.title)
+			.setURL(`https://www.reddit.com${data.permalink}`)
+			.setImage(data.url)
+			.setFooter(data.subreddit_name_prefixed)
+			.setTimestamp(data.created));
 	}
 
 };
