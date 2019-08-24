bf2a9d60b5d48ebb80e0063e85382e8f6c9fbe01
Dwigoric <dwigoric@gmail.com>
2019-08-24 11:46:27 +0800

    Fix afkChannelOnAfk

--- src/events/afkChannelOnAfk.js
+++ src/events/afkChannelOnAfk.js
@@ -14,6 +14,8 @@ module.exports = class extends Event {
 		if (!newState.member.voice.channelID) return null;
 		if (!newState.guild.afkChannelID) return null;
 		if (newState.channelID === newState.guild.afkChannelID) return null;
+		const afkUsers = await this.client.providers.default.getKeys('afk');
+		if (!afkUsers.includes(newState.member.user.id)) return null;
 		return newState.setChannel(newState.guild.afkChannelID, 'Moved to AFK channel due to AFK status');
 	}
 
