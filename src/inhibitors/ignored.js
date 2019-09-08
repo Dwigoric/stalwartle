aa4721a4ebea964f6af1e17f3b66be7367730e5f
Dwigoric <dwigoric@gmail.com>
2019-09-08 09:06:28 +0800

    Use string for ignore notification

--- src/inhibitors/ignored.js
+++ src/inhibitors/ignored.js
@@ -5,7 +5,7 @@ module.exports = class extends Inhibitor {
 	async run(msg, cmd) {
 		if (!msg.guild) return;
 		if (cmd.name === 'ignore') return;
-		if (msg.guild.settings.get('ignored').includes(msg.channel.id)) throw msg.send('🔇  ::  This channel is included in this server\'s ignored channels.');
+		if (msg.guild.settings.get('ignored').includes(msg.channel.id)) throw '🔇  ::  This channel is included in this server\'s ignored channels.';
 	}
 
 };
