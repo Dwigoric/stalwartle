f23084a33f4a0c02ed011b4b437d40e7330c6726
Dwigoric <dwigoric@gmail.com>
2019-09-18 22:24:27 +0800

    Wait until the client is ready before sending message

--- src/events/klasaReady.js
+++ src/events/klasaReady.js
@@ -39,6 +39,7 @@ module.exports = class extends Event {
 		});
 		const { channel, timestamp } = (await this.client.settings.resolve('restart'))[0];
 		if (!channel) return;
+		while (!this.client.ready) continue;
 		this.client.channels.get(channel).send({
 			embed: new MessageEmbed()
 				.setColor(0x40E0D0)
