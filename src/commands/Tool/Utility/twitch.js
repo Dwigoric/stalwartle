9ae629d713caf27412a9a0f3ca575264788206a0
Dwigoric <dwigoric@gmail.com>
2019-12-17 17:53:27 +0800

    Update twitch command for new Twitch API

--- src/commands/Tool/Utility/twitch.js
+++ src/commands/Tool/Utility/twitch.js
@@ -1,4 +1,4 @@
-const { Command, Timestamp } = require('klasa');
+const { Command } = require('klasa');
 const { MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
 
@@ -10,29 +10,28 @@ module.exports = class extends Command {
 			description: 'Returns information on a Twitch.tv Account',
 			usage: '<TwitchName:string>'
 		});
-		this.timestamp = new Timestamp('DD-MM-YYYY');
 	}
 
 	async run(msg, [twitchName]) {
 		await msg.send('<a:loading:430269209415516160>  ::  Loading Twitch channel...');
 
-		const channel = await fetch(`https://api.twitch.tv/kraken/channels/${twitchName}?client_id=${this.client.auth.twitchAPIkey}`)
+		const channel = await fetch(`https://api.twitch.tv/helix/users?login=${twitchName}`, {
+			headers: { 'Client-ID': this.client.auth.twitchAPIkey }
+		})
 			.then(res => res.json())
 			.then(res => {
-				if (res.error) throw `<:error:508595005481549846>  ::  ${res.message}.`;
-				return res;
+				if (!res.data.length) throw `<:error:508595005481549846>  ::  There is no Twitch streamer named **${twitchName}**.`;
+				return res.data[0];
 			});
 
-		const creationDate = this.timestamp.display(channel.created_at);
 		return msg.sendEmbed(new MessageEmbed()
 			.setColor(6570406)
-			.setThumbnail(channel.logo)
-			.setAuthor(channel.display_name, 'https://i.imgur.com/OQwQ8z0.jpg', channel.url)
+			.setThumbnail(channel.profile_image_url)
+			.setAuthor(channel.display_name, 'https://i.imgur.com/OQwQ8z0.jpg', `https://twitch.tv/${channel.display_name}`)
+			.setDescription(channel.description)
 			.setFooter('Click at the account name above to go to the channel.')
-			.addField('Account ID', channel._id, true)
-			.addField('Followers', channel.followers, true)
-			.addField('Created On', creationDate, true)
-			.addField('Channel Views', channel.views, true));
+			.addField('Account ID', channel.id, true)
+			.addField('Channel Views', channel.view_count, true));
 	}
 
 };
