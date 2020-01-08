0df88a46a532728cea8da79fcae5837e7ba025bb
TheFloppyBanana <angelolanceseraspi@gmail.com>
2020-01-08 14:29:26 +0800

    fix: catch when muterole command is unable to create a role

--- src/commands/Moderation/Server Management/muterole.js
+++ src/commands/Moderation/Server Management/muterole.js
@@ -38,7 +38,8 @@ module.exports = class extends Command {
 				color: 'DARKER_GREY',
 				permissions: 0
 			}
-		});
+		}).catch(() => null);
+		if (!newRole) throw '<:error:508595005481549846>  ::  I cannot create the muted role. Please double check my permissions';
 		msg.guild.settings.update('muteRole', newRole.id, { guild: msg.guild });
 		return msg.send(`<:check:508594899117932544>  ::  Successfully set this server's mute role to **${newRole.name}**.`);
 	}
