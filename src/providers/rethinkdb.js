6d68d5402138543e6cef16fed8862a9292fa8de5
Dwigoric <dwigoric@gmail.com>
2020-04-24 12:38:39 +0800

    misc(rethinkdb): add copyright

--- src/providers/rethinkdb.js
+++ src/providers/rethinkdb.js
@@ -1,3 +1,4 @@
+// Copyright (c) 2017-2019 dirigeants. All rights reserved. MIT license.
 const { Provider, util: { mergeDefault, chunk } } = require('klasa');
 const { r } = require('rethinkdb-ts'); // eslint-disable-line id-length
 
