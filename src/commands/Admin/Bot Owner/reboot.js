d687f43401b64e11b9a4636408c73ee5ec11f6b0
Dwigoric <dwigoric@gmail.com>
2020-06-08 18:19:11 +0800

    misc: move reboot message from klasaReady to reboot init

--- src/commands/Admin/Bot Owner/reboot.js
+++ src/commands/Admin/Bot Owner/reboot.js
@@ -1,4 +1,5 @@
 const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = class extends Command {
 
@@ -15,6 +16,21 @@ module.exports = class extends Command {
 		await msg.sendLocale('COMMAND_REBOOT').catch(err => this.client.emit('error', err));
 		await this.client.destroy();
 		process.exit();
+	}
+
+	async init() {
+		const { authority, timestamp } = await this.client.settings.get('restart');
+		if (!authority) return;
+		(await this.client.users.fetch(authority)).send({
+			embed: new MessageEmbed()
+				.setColor(0x40E0D0)
+				.setTitle('Bot has successfully restarted!')
+				.setThumbnail(this.client.user.displayAvatarURL())
+				.setDescription(`**Creeping through Discord...**\nand doing some magic!\n\nCurrently running on **${await this.client.guildCount()}** guilds with **${await this.client.userCount()}** users.`) // eslint-disable-line max-len
+				.setFooter(`Reboot duration: ${+`${`${Math.round(`${`${(Date.now() - timestamp) / 1000}e+2`}`)}e-2`}`}s`)
+				.setTimestamp()
+		});
+		this.client.settings.reset(['restart.authority', 'restart.timestamp']);
 	}
 
 };
--- src/events/klasaReady.js
+++ src/events/klasaReady.js
@@ -1,5 +1,4 @@
 const { Event } = require('klasa');
-const { MessageEmbed } = require('discord.js');
 
 const statuses = [
 	{ name: 'dead', type: 'PLAYING' },
@@ -35,18 +34,6 @@ module.exports = class extends Event {
 				this.client.user.setActivity(`${status.name} | ${this.client.options.prefix}help`, { type: status.type });
 			}, 60000);
 		});
-		const { authority, timestamp } = await this.client.settings.get('restart');
-		if (!authority) return;
-		(await this.client.users.fetch(authority)).send({
-			embed: new MessageEmbed()
-				.setColor(0x40E0D0)
-				.setTitle('Bot has successfully restarted!')
-				.setThumbnail(this.client.user.displayAvatarURL())
-				.setDescription(`**Creeping through Discord...**\nand doing some magic!\n\nCurrently running on **${await this.client.guildCount()}** guilds with **${await this.client.userCount()}** users.`) // eslint-disable-line max-len
-				.setFooter(`Reboot duration: ${+`${`${Math.round(`${`${(Date.now() - timestamp) / 1000}e+2`}`)}e-2`}`}s`)
-				.setTimestamp()
-		});
-		this.client.settings.reset(['restart.authority', 'restart.timestamp']);
 	}
 
 };
