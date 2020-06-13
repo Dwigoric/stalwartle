b757ec79018686f862bbbf836283215043136c30
Dwigoric <dwigoric@gmail.com>
2020-06-13 16:20:28 +0800

    fix(anime/manga): use jikan API

--- src/commands/Tool/Utility/anime.js
+++ src/commands/Tool/Utility/anime.js
@@ -1,7 +1,6 @@
 const { Command } = require('klasa');
 const { MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
-const cheerio = require('cheerio');
 
 module.exports = class extends Command {
 
@@ -16,34 +15,23 @@ module.exports = class extends Command {
 
 	async run(msg, [keyword]) {
 		await msg.send('<a:loading:430269209415516160>  ::  Loading anime...');
-		const search = await fetch(`https://myanimelist.net/search/prefix.json?type=anime&keyword=${encodeURIComponent(keyword)}`)
+		const search = await fetch(`https://api.jikan.moe/v3/search/anime?q=${encodeURIComponent(keyword)}&limit=1`)
 			.then(res => res.json())
-			.then(body => body.categories[0]);
-		if (!search || !search.items || !search.items.length) throw '<:error:508595005481549846>  ::  Anime series not found!';
+			.then(body => body.results);
+		if (!search.length) throw '<:error:508595005481549846>  ::  Anime series not found!';
 
-		const url = search.items[0].url.split('/');
-		url.splice(-1, 1, encodeURIComponent(url.slice(-1)));
-		const $ = cheerio.load(await fetch(url.join('/')).then(res => res.text())); // eslint-disable-line id-length
-		const anime = {
-			url: search.items[0].url,
-			title: $('h1').text().trim(),
-			description: $('span[itemprop=description]').text(),
-			score: parseFloat($('.fl-l.score').text()),
-			popularity: parseInt($('.numbers.popularity strong').text().substring(1)),
-			ranked: parseInt($('.numbers.ranked strong').text().substring(1)),
-			cover: $('.ac').eq(0).attr('src')
-		};
+		const [anime] = search;
 
 		msg.send({
 			embed: new MessageEmbed()
 				.setColor('RANDOM')
 				.setTitle(anime.title)
-				.setThumbnail(anime.cover)
-				.setDescription(anime.description)
+				.setThumbnail(anime.image_url)
+				.setDescription(anime.synopsis)
 				.setURL(anime.url)
+				.addField('Episodes', anime.episodes, true)
+				.addField('Rating', anime.rated, true)
 				.addField('Score', anime.score, true)
-				.addField('Popularity', anime.popularity, true)
-				.addField('Rank', anime.ranked, true)
 		});
 	}
 
--- src/commands/Tool/Utility/manga.js
+++ src/commands/Tool/Utility/manga.js
@@ -1,7 +1,6 @@
 const { Command } = require('klasa');
 const { MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
-const cheerio = require('cheerio');
 
 module.exports = class extends Command {
 
@@ -16,34 +15,23 @@ module.exports = class extends Command {
 
 	async run(msg, [keyword]) {
 		await msg.send('<a:loading:430269209415516160>  ::  Loading manga...');
-		const search = await fetch(`https://myanimelist.net/search/prefix.json?type=manga&keyword=${encodeURIComponent(keyword)}`)
+		const search = await fetch(`https://api.jikan.moe/v3/search/manga?q=${encodeURIComponent(keyword)}&limit=1`)
 			.then(res => res.json())
-			.then(body => body.categories[0]);
+			.then(body => body.results[0]);
 		if (!search || (Array.isArray(search) && !search.length)) throw '<:error:508595005481549846>  ::  Manga not found!';
 
-		const url = search.items[0].url.split('/');
-		url.splice(-1, 1, encodeURIComponent(url.slice(-1)));
-		const $ = cheerio.load(await fetch(url.join('/')).then(res => res.text())); // eslint-disable-line id-length
-		const manga = {
-			url: search.items[0].url,
-			title: $('h1').text().trim(),
-			description: $('span[itemprop=description]').text(),
-			score: parseFloat($('.fl-l.score').text()),
-			popularity: parseInt($('.numbers.popularity strong').text().substring(1)),
-			ranked: parseInt($('.numbers.ranked strong').text().substring(1)),
-			cover: $('.ac').eq(0).attr('src')
-		};
+		const [manga] = search;
 
 		msg.send({
 			embed: new MessageEmbed()
 				.setColor('RANDOM')
 				.setTitle(manga.title)
-				.setThumbnail(manga.cover)
-				.setDescription(manga.description)
+				.setThumbnail(manga.image_url)
+				.setDescription(manga.synopsis)
 				.setURL(manga.url)
+				.addField('Volumes', manga.volumes, true)
+				.addField('Chapters', manga.chapters, true)
 				.addField('Score', manga.score, true)
-				.addField('Popularity', manga.popularity, true)
-				.addField('Rank', manga.ranked, true)
 		});
 	}
 
