265c35b899878a3efbe61dfdc21fe944788ea02e
Dwigoric <dwigoric@gmail.com>
2020-08-18 09:41:28 +0800

    refactor(banner): add some checks

--- src/commands/Fun/Not Insane/banner.js
+++ src/commands/Fun/Not Insane/banner.js
@@ -12,6 +12,8 @@ module.exports = class extends Command {
 
 	async run(msg, [banner]) {
 		const data = await figletAsync(banner);
+		if (data.length > 2000) throw `${this.client.constants.EMOTES.xmark}  ::  The banner was too long! Please try making it shorter.`;
+		if (!data) throw `${this.client.constants.EMOTES.xmark}  ::  Something went wrong! Did you supply a non-alphanumeric character?`;
 		return msg.sendCode('', data);
 	}
 
