eb1e5c45311ead5587529a6c7b0b885613859f10
Dwigoric <dwigoric@gmail.com>
2020-08-23 17:29:49 +0800

    refactor(volume): check player existence to change volume

--- src/commands/Music/Control/volume.js
+++ src/commands/Music/Control/volume.js
@@ -15,7 +15,7 @@ module.exports = class extends Command {
 	async run(msg, [volume]) {
 		if (!volume) return msg.send(`🎚  ::  The volume for this server is currently set to ${msg.guild.settings.get('music.volume')}%.`);
 		msg.guild.settings.update('music.volume', volume);
-		if (msg.guild.me.voice.channelID) msg.guild.player.volume(volume);
+		if (msg.guild.player) msg.guild.player.volume(volume);
 		return msg.send(`${this.client.constants.EMOTES.tick}  ::  Successfully changed the volume for this server to ${volume}%.`);
 	}
 
