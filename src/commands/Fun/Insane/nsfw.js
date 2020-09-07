a400a78a546861978848eede7e95c504430f56e2
Dwigoric <dwigoric@gmail.com>
2020-09-07 08:38:09 +0800

    misc(nsfw): include source URL

--- src/commands/Fun/Insane/nsfw.js
+++ src/commands/Fun/Insane/nsfw.js
@@ -15,7 +15,7 @@ module.exports = class extends Command {
 	async run(msg) {
 		const message = await msg.send(`${this.client.constants.EMOTES.loading}  ::  Loading image...`);
 		const result = await fetch(`https://api.ksoft.si/images/random-nsfw?gifs=${Boolean(msg.flagArgs.gif)}`, { headers: { Authorization: `Bearer ${this.client.auth.ksoftAPIkey}` } }).then(res => res.json()); // eslint-disable-line max-len
-		await msg.channel.sendFile(result.image_url);
+		await msg.channel.sendFile(result.image_url, undefined, `<${result.source}>`);
 		message.delete();
 	}
 
