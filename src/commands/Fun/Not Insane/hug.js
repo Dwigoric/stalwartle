9996e33a8bbed2ba1fbb4eb8b783f589a3fb4779
Dwigoric <dwigoric@gmail.com>
2020-09-30 11:01:57 +0800

    refactor(hug): remove gif from embed

--- src/commands/Fun/Not Insane/hug.js
+++ src/commands/Fun/Not Insane/hug.js
@@ -14,13 +14,12 @@ module.exports = class extends Command {
 	}
 
 	async run(msg, [person]) {
-		await msg.send(`${this.client.constants.EMOTES.loading}  ::  Loading GIF...`);
+		const message = await msg.send(`${this.client.constants.EMOTES.loading}  ::  Loading GIF...`);
 
 		const { link } = await fetch(`https://some-random-api.ml/animu/hug`).then(res => res.json());
-		const embed = new MessageEmbed()
-			.setColor('RANDOM')
-			.setImage(link);
-		return msg.send(`🤗  ::  **${msg.member.displayName}** wants to hug ${person}!`, { embed });
+		msg.channel.sendFile(link, 'hug.gif', `🤗  ::  **${msg.member.displayName}** wants to hug ${person}!`);
+
+		message.delete();
 	}
 
 };
