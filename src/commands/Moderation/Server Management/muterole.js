7dbcdee1ad0e9d356b628bf06d4b5a83f7f59f9d
Dwigoric <dwigoric@gmail.com>
2020-12-16 08:04:11 +0800

    refactor(muterole): disallow using existing role as muterole

--- src/commands/Moderation/Server Management/muterole.js
+++ src/commands/Moderation/Server Management/muterole.js
@@ -8,22 +8,22 @@ module.exports = class extends Command {
 			runIn: ['text'],
 			requiredPermissions: 'MANAGE_ROLES',
 			description: 'Sets the mute role for the server.',
-			extendedHelp: 'You can set an existing role as the mute role; or I can make one for you using the name you provide.',
-			usage: '[reset] <Role:role|RoleName:string> [...]',
+			extendedHelp: 'Set the server\'s mute role using the name you provide.',
+			usage: '[reset] <RoleName:string> [...]',
 			usageDelim: ' ',
 			subcommands: true
 		});
 	}
 
 	async run(msg, [...role]) {
-		const newRole = role[0] || await msg.guild.roles.create({
+		const newRole = await msg.guild.roles.create({
 			data: {
 				name: role.join(this.usageDelim),
 				color: 'DARKER_GREY',
 				permissions: 0
 			}
 		}).catch(() => null);
-		if (!newRole) throw `${this.client.constants.EMOTES.xmark}  ::  I cannot create the muted role. Please double check my permissions`;
+		if (!newRole) throw `${this.client.constants.EMOTES.xmark}  ::  I cannot create the muted role. Please double-check my permissions.`;
 		msg.guild.settings.update('muteRole', newRole.id, msg.guild);
 		return msg.send(`${this.client.constants.EMOTES.tick}  ::  Successfully set this server's mute role to **${newRole.name}**.`);
 	}
