99718dbc67bf27cddbf0caa22687c7135ff4d1d5
Joshua Permito <dwigoric@insanedevs.com>
2021-08-10 19:26:03 +0800

    refactor(eval): remove type section

--- src/commands/Admin/Bot Owner/eval.js
+++ src/commands/Admin/Bot Owner/eval.js
@@ -21,47 +21,46 @@ module.exports = class extends Command {
 	async run(msg, [code]) {
 		const flagTime = 'no-timeout' in msg.flagArgs ? 'wait' in msg.flagArgs ? Number(msg.flagArgs.wait) : this.timeout : Infinity;
 		const language = msg.flagArgs.lang || msg.flagArgs.language || (msg.flagArgs.json ? 'json' : 'js');
-		const { success, result, time, type } = await this.timedEval(msg, code, flagTime);
+		const { success, result, time } = await this.timedEval(msg, code, flagTime);
 
 		if (msg.flagArgs.silent) {
 			if (!success && result && result.stack) this.client.emit('error', result.stack);
 			return null;
 		}
 
-		const footer = util.codeBlock('ts', type);
 		const sendAs = msg.flagArgs.output || msg.flagArgs['output-to'] || (msg.flagArgs.log ? 'log' : null);
-		return this.handleMessage(msg, { sendAs, hastebinUnavailable: false, url: null }, { success, result, time, footer, language });
+		return this.handleMessage(msg, { sendAs, hastebinUnavailable: false, url: null }, { success, result, time, language });
 	}
 
-	async handleMessage(msg, options, { success, result, time, footer, language }) {
+	async handleMessage(msg, options, { success, result, time, language }) {
 		switch (options.sendAs) {
 			case 'file': {
-				if (msg.channel.attachable) return msg.channel.sendFile(Buffer.from(result), 'output.txt', msg.language.get('COMMAND_EVAL_OUTPUT_FILE', time, footer));
+				if (msg.channel.attachable) return msg.channel.sendFile(Buffer.from(result), 'output.txt', msg.language.get('COMMAND_EVAL_OUTPUT_FILE', time));
 				await this.getTypeOutput(msg, options);
-				return this.handleMessage(msg, options, { success, result, time, footer, language });
+				return this.handleMessage(msg, options, { success, result, time, language });
 			}
 			case 'haste':
 			case 'hastebin': {
 				if (!options.url) options.url = await this.getHaste(result, language).catch(() => null);
-				if (options.url) return msg.sendMessage(msg.language.get('COMMAND_EVAL_OUTPUT_HASTEBIN', time, options.url, footer));
+				if (options.url) return msg.sendMessage(msg.language.get('COMMAND_EVAL_OUTPUT_HASTEBIN', time, options.url));
 				options.hastebinUnavailable = true;
 				await this.getTypeOutput(msg, options);
-				return this.handleMessage(msg, options, { success, result, time, footer, language });
+				return this.handleMessage(msg, options, { success, result, time, language });
 			}
 			case 'console':
 			case 'log': {
 				this.client.emit('log', result);
-				return msg.sendMessage(msg.language.get('COMMAND_EVAL_OUTPUT_CONSOLE', time, footer));
+				return msg.sendMessage(msg.language.get('COMMAND_EVAL_OUTPUT_CONSOLE', time));
 			}
 			case 'none':
 				return null;
 			default: {
 				if (result.length > 2000) {
 					await this.getTypeOutput(msg, options);
-					return this.handleMessage(msg, options, { success, result, time, footer, language });
+					return this.handleMessage(msg, options, { success, result, time, language });
 				}
 				return msg.sendMessage(msg.language.get(success ? 'COMMAND_EVAL_OUTPUT' : 'COMMAND_EVAL_ERROR',
-					time, util.codeBlock(language, result), footer));
+					time, util.codeBlock(language, result)));
 			}
 		}
 	}
