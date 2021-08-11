4c28a0878877caf769c24145d8319648716c2cbe
Joshua Permito <dwigoric@insanedevs.com>
2021-08-11 14:07:24 +0800

    fix(history): skip if no history

--- src/tasks/shiftHistory.js
+++ src/tasks/shiftHistory.js
@@ -4,6 +4,7 @@ module.exports = class extends Task {
 
 	async run() {
 		for (const { history, id } of await this.client.providers.default.getAll('music')) {
+			if (!history) continue;
 			this.client.providers.default.update('music', id, { history: history.filter(hist => (Date.now() - hist.timestamp) <= (1000 * 60 * 60 * 24)) });
 		}
 
