6e4b3073276b89b7f84543117946c58014ae2cd4
Joshua Permito <dwigoric@insanedevs.com>
2021-08-11 19:12:50 +0800

    fix(modlogs): use modlogs gateway

--- src/lib/structures/StalwartleGuild.js
+++ src/lib/structures/StalwartleGuild.js
@@ -7,7 +7,7 @@ module.exports = Structures.extend('Guild', Guild => {
 			super(...args);
 			this.voteskips = [];
 			this.music = this.client.gateways.music.get(this.id, true);
-			this.modlogs = this.client.gateways.music.get(this.id, true);
+			this.modlogs = this.client.gateways.modlogs.get(this.id, true);
 		}
 
 	}
