eb8b4327ba9ca964832d2a1c80531ce6282b6ac7
Joshua Permito <dwigoric@insanedevs.com>
2021-08-11 21:35:16 +0800

    fix: return cache entry itself

--- src/lib/structures/settings/Gateway.js
+++ src/lib/structures/settings/Gateway.js
@@ -25,7 +25,7 @@ module.exports = class Gateway {
 
 	get(id, create = false) {
 		const entry = this.cache.get(id);
-		if (entry) return entry.settings;
+		if (entry) return entry;
 		if (create) {
 			const settings = new this.DataStore(this, { id });
 			if (this._synced && this.schema.size) settings.sync(true).catch(err => this.client.emit('error', err));
