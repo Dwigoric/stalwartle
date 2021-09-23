0ae1f8a1ac61dc5b9962cf041fe3ac314793ebc5
Joshua Permito <dwigoric@insanedevs.com>
2021-09-24 07:31:23 +0800

    refactor: restrict music control commands

--- src/preconditions/DJOnly.js
+++ src/preconditions/DJOnly.js
@@ -2,8 +2,9 @@ const { Precondition } = require('@sapphire/framework');
 
 module.exports = class DJOnlyPrecondition extends Precondition {
 
-    async run(msg) {
+    async run(msg, cmd) {
         if (!msg.member) return this.ok();
+        if (cmd.fullCategory !== ['Music', 'Control']) return this.ok();
         const musicGateway = this.container.client.gateways.music.get(msg.guild.id);
         if (!musicGateway.music.dj.length) return this.ok();
         if (musicGateway.music.dj.some(role => msg.member.roles.cache.keyArray().includes(role))) return this.ok();
--- src/preconditions/MusicControl.js
+++ src/preconditions/MusicControl.js
@@ -2,9 +2,9 @@ const { Precondition } = require('@sapphire/framework');
 
 module.exports = class MusicControlPrecondition extends Precondition {
 
-    async run(msg, command) {
+    async run(msg, cmd) {
         if (!msg.guild) return this.ok();
-        if (!command.path.includes('Music/Control')) return this.ok();
+        if (cmd.fullCategory !== ['Music', 'Control']) return this.ok();
         if (!msg.guild.me.voice.channel) return this.ok();
         if (!msg.guild.me.voice.channel.members.has(msg.member.id)) return this.error({ message: `${this.container.client.constants.EMOTES.xmark}  ::  You must be connected to #**${msg.guild.me.voice.channel.name}** to be able to control the music session.` }); // eslint-disable-line max-len
         return this.ok();
