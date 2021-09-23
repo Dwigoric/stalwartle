f1b53511d653b167b2867bc0ec972e0c5e7971fa
Joshua Permito <dwigoric@insanedevs.com>
2021-09-24 07:31:23 +0800

    Revert "refactor: restrict music control commands"
    This reverts commit 0ae1f8a1ac61dc5b9962cf041fe3ac314793ebc5.

--- src/preconditions/DJOnly.js
+++ src/preconditions/DJOnly.js
@@ -2,9 +2,8 @@ const { Precondition } = require('@sapphire/framework');
 
 module.exports = class DJOnlyPrecondition extends Precondition {
 
-    async run(msg, cmd) {
+    async run(msg) {
         if (!msg.member) return this.ok();
-        if (cmd.fullCategory !== ['Music', 'Control']) return this.ok();
         const musicGateway = this.container.client.gateways.music.get(msg.guild.id);
         if (!musicGateway.music.dj.length) return this.ok();
         if (musicGateway.music.dj.some(role => msg.member.roles.cache.keyArray().includes(role))) return this.ok();
