52e8d3ef4a95a1d3127351e330335a052bcc8579
Joshua Permito <dwigoric@insanedevs.com>
2022-01-14 11:26:19 +0800

    fix(ping): use editable commands plugin

--- src/commands/General/Chat Bot Info/ping.js
+++ src/commands/General/Chat Bot Info/ping.js
@@ -1,4 +1,5 @@
 const { Command } = require('@sapphire/framework');
+const { send } = require('@sapphire/plugin-editable-commands');
 
 module.exports = class extends Command {
 
@@ -10,8 +11,8 @@ module.exports = class extends Command {
     }
 
     async messageRun(msg) {
-        const message = await msg.send(`🏓  ::  **Pong!**`);
-        msg.send(`🏓  ::  **Pong!** Ping pong game ended! 😃 ~~(I won)~~ | Game Duration: **${(message.editedTimestamp || message.createdTimestamp) -
+        const message = await send(msg, `🏓  ::  **Pong!**`);
+        send(msg, `🏓  ::  **Pong!** Ping pong game ended! 😃 ~~(I won)~~ | Game Duration: **${(message.editedTimestamp || message.createdTimestamp) -
 			(msg.editedTimestamp || msg.createdTimestamp)}ms**. API Latency: **${Math.round(this.container.client.ws.ping)}ms**.`);
     }
 
