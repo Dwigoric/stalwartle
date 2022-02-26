041b87c3cb8b8ba498a3873d9543025bbbd195b9
Joshua Permito <dwigoric@insanedevs.com>
2022-02-26 20:21:28 +0800

    refactor: allow admins on DJOnly and ModsOnly

--- src/preconditions/DJOnly.js
+++ src/preconditions/DJOnly.js
@@ -4,6 +4,7 @@ module.exports = class DJOnlyPrecondition extends Precondition {
 
     async run(msg) {
         if (!msg.member) return this.ok();
+        if (msg.member.permissions.has('ADMINISTRATOR')) return this.ok();
         const musicGateway = this.container.stores.get('gateways').get('musicGateway').get(msg.guild.id);
         if (!musicGateway.music.dj.length) return this.ok();
         if (musicGateway.music.dj.some(role => msg.member.roles.cache.keyArray().includes(role))) return this.ok();
--- src/preconditions/ModsOnly.js
+++ src/preconditions/ModsOnly.js
@@ -3,7 +3,8 @@ const { Precondition } = require('@sapphire/framework');
 module.exports = class ModsOnlyPrecondition extends Precondition {
 
     async run(msg) {
-        if (!msg.member) return this.ok();
+        if (!msg.member) return this.error();
+        if (msg.member.permissions.has('ADMINISTRATOR')) return this.ok();
         const musicGateway = this.container.stores.get('gateways').get('musicGateway').get(msg.guild.id);
         if (musicGateway.moderators.roles.some(role => msg.member.roles.cache.keyArray().includes(role))) return this.ok();
         if (musicGateway.moderators.users.includes(msg.author.id)) return this.ok();
