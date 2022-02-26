3b41aa50e339cc2ae26a2765c657d7f373269473
Joshua Permito <dwigoric@insanedevs.com>
2022-02-26 15:19:18 +0800

    fix(reboot): update to djs v13 and use gateways

--- src/commands/Admin/Bot Owner/reboot.js
+++ src/commands/Admin/Bot Owner/reboot.js
@@ -16,7 +16,7 @@ module.exports = class extends Command {
         await this.container.stores.get('gateways').get('clientGateway').update(this.container.client.user.id, { restart: { channel: msg.channel.id, timestamp: msg.createdTimestamp } });
         await msg.channel.send('<a:loading:430269209415516160>  ::  Bot is restarting... I will message you in this channel once I\'ve woken up again.').catch(err => this.container.client.emit('error', err));
         await this.container.client.destroy();
-        await this.container.client.playerManager.destroy();
+        await this.container.lavacord.disconnect();
         process.exit();
     }
 
@@ -24,18 +24,15 @@ module.exports = class extends Command {
         const { channel, timestamp } = await this.container.client.settings.restart;
         if (!channel) return;
         (await this.container.client.channels.fetch(channel)).send({
-            embed: new MessageEmbed()
+            embeds: [new MessageEmbed()
                 .setColor(0x40E0D0)
                 .setTitle('Bot has successfully restarted!')
                 .setThumbnail(this.container.client.user.displayAvatarURL({ dynamic: true }))
                 .setDescription(`**Creeping through Discord...**\nand doing some magic!\n\nCurrently running on **${await this.container.client.guildCount()}** guilds with **${await this.container.client.userCount()}** users.`) // eslint-disable-line max-len
-                .setFooter(`Reboot duration: ${+`${`${Math.round(`${`${(Date.now() - timestamp) / 1000}e+2`}`)}e-2`}`}s`)
-                .setTimestamp()
+                .setFooter({ text: `Reboot duration: ${+`${`${Math.round(`${`${(Date.now() - timestamp) / 1000}e+2`}`)}e-2`}`}s` })
+                .setTimestamp()]
         });
-        this.container.stores.get('gateways').get('clientGateway').update(this.container.client.user.id, { restart: {
-            channel: this.container.stores.get('gateways').get('clientGateway').defaults.channel,
-            timestamp: this.container.stores.get('gateways').get('clientGateway').defaults.timestamp
-        } });
+        ['restart.channel', 'restart.timestamp'].forEach(path => this.container.stores.get('gateways').get('clientGateway').reset(this.container.client.id, path));
     }
 
 };
