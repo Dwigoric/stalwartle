b0a84fed591646fc2dbfc851dead7ca1acdb4ada
Joshua Permito <dwigoric@insanedevs.com>
2022-02-27 08:57:27 +0800

    fix(ModsOnly): use guild gateway

--- src/preconditions/ModsOnly.js
+++ src/preconditions/ModsOnly.js
@@ -5,9 +5,9 @@ module.exports = class ModsOnlyPrecondition extends Precondition {
     async run(msg) {
         if (!msg.member) return this.error();
         if (msg.member.permissions.has('ADMINISTRATOR')) return this.ok();
-        const musicGateway = this.container.stores.get('gateways').get('musicGateway').get(msg.guild.id);
-        if (musicGateway.moderators.roles.some(role => msg.member.roles.cache.keyArray().includes(role))) return this.ok();
-        if (musicGateway.moderators.users.includes(msg.author.id)) return this.ok();
+        const guildGateway = this.container.stores.get('gateways').get('guildGateway').get(msg.guild.id);
+        if (guildGateway.moderators.roles.some(role => msg.member.roles.cache.keyArray().includes(role))) return this.ok();
+        if (guildGateway.moderators.users.includes(msg.author.id)) return this.ok();
         return this.error();
     }
 
