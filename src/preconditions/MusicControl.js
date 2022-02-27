587c9f034b740cc8138264adf84f5286efa9fae5
Joshua Permito <dwigoric@insanedevs.com>
2022-02-28 07:14:30 +0800

    fix(MusicContorl): use command category accessors

--- src/preconditions/MusicControl.js
+++ src/preconditions/MusicControl.js
@@ -4,7 +4,7 @@ module.exports = class MusicControlPrecondition extends Precondition {
 
     async run(msg, command) {
         if (!msg.guild) return this.ok();
-        if (!command.path.includes('Music/Control')) return this.ok();
+        if (command.category !== 'Music' || (command.category === 'Music' && command.subCategory !== 'Control')) return this.ok();
         if (!msg.guild.me.voice.channel) return this.ok();
         if (!msg.guild.me.voice.channel.members.has(msg.member.id)) return this.error({ message: `${this.container.constants.EMOTES.xmark}  ::  You must be connected to #**${msg.guild.me.voice.channel.name}** to be able to control the music session.` }); // eslint-disable-line max-len
         return this.ok();
