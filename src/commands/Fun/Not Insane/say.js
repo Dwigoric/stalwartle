a3308e5eeee94c3ead558f425871e70267869bc5
Joshua Permito <dwigoric@insanedevs.com>
2022-02-28 18:30:15 +0800

    fix(say): migrate say command to Sapphire

--- src/commands/Fun/Not Insane/say.js
+++ src/commands/Fun/Not Insane/say.js
@@ -1,38 +1,52 @@
 const { Command } = require('@sapphire/framework');
+const { reply } = require('@sapphire/plugin-editable-commands');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = class extends Command {
 
-    constructor(...args) {
-        super(...args, {
-            requiredPermissions: ['EMBED_LINKS'],
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            requiredClientPermissions: 'EMBED_LINKS',
             description: 'Makes the bot say anything you want.',
             usage: '[delete|embed|anonymous] [Channel:channel] <Content:string{1,1000}> [...]',
             usageDelim: ' ',
-            subcommands: true
+            subcommands: ['delete', 'embed', 'anonymous']
         });
     }
 
-    async messageRun(msg, [chan = msg.channel, ...msgargs]) {
-        msgargs = msgargs.join(this.usageDelim);
-        if (!chan.postable) throw `${this.container.constants.EMOTES.xmark}  ::  Sorry! I cannot send messages in that channel.`; // eslint-disable-line max-len
-        if (chan !== msg.channel) msg.send(`${this.container.constants.EMOTES.tick}  ::  Message sent!`);
-        return chan.reply(msgargs);
+    async messageRun(msg, args) {
+        const chan = await args.pick('guildTextChannel').catch(() => msg.channel);
+        let msgargs = await args.restResult('string');
+        if (!msgargs) return reply(msg, `${this.container.constants.EMOTES.xmark}  ::  Please provide the message you want to say.`);
+        msgargs = msgargs.value;
+
+        if (!chan.permissionsFor(this.client.user).has('SEND_MESSAGES')) throw `${this.container.constants.EMOTES.xmark}  ::  Sorry! I cannot send messages in that channel.`;
+        if (chan !== msg.channel) reply(msg, `${this.container.constants.EMOTES.tick}  ::  Message sent!`);
+        return chan.send(msgargs);
     }
 
-    async delete(msg, [chan = msg.channel, ...msgargs]) {
+    async delete(msg, args) {
+        const chan = await args.pick('guildTextChannel').catch(() => msg.channel);
+        let msgargs = await args.restResult('string');
+        if (!msgargs) return reply(msg, `${this.container.constants.EMOTES.xmark}  ::  Please provide the message you want to say.`);
+        msgargs = msgargs.value;
+
         if (!msg.guild || !msg.channel.permissionsFor(this.container.client.user).has('MANAGE_MESSAGES')) throw `${this.container.constants.EMOTES.xmark}  ::  Sorry! I cannot delete messages in this channel.`;
-        msgargs = msgargs.join(this.usageDelim);
-        if (!chan.postable) throw `${this.container.constants.EMOTES.xmark}  ::  Sorry! I cannot send messages in that channel.`;
-        if (chan !== msg.channel) msg.send(`${this.container.constants.EMOTES.tick}  ::  Message sent!`);
-        chan.reply(msgargs);
+        if (!chan.permissionsFor(this.client.user).has('SEND_MESSAGES')) throw `${this.container.constants.EMOTES.xmark}  ::  Sorry! I cannot send messages in that channel.`;
+        if (chan !== msg.channel) reply(msg, `${this.container.constants.EMOTES.tick}  ::  Message sent!`);
+        chan.send(msgargs);
         return msg.delete().catch(() => null);
     }
 
-    async embed(msg, [chan = msg.channel, ...msgargs]) {
-        msgargs = msgargs.join(this.usageDelim);
-        if (!chan.postable) throw `${this.container.constants.EMOTES.xmark}  ::  Sorry! I cannot send messages in that channel.`;
-        if (chan !== msg.channel) msg.send(`${this.container.constants.EMOTES.tick}  ::  Message sent!`);
+    async embed(msg, args) {
+        const chan = await args.pick('guildTextChannel').catch(() => msg.channel);
+        let msgargs = await args.restResult('string');
+        if (!msgargs) return reply(msg, `${this.container.constants.EMOTES.xmark}  ::  Please provide the message you want to say.`);
+        msgargs = msgargs.value;
+
+        if (!chan.permissionsFor(this.client.user).has('SEND_MESSAGES')) throw `${this.container.constants.EMOTES.xmark}  ::  Sorry! I cannot send messages in that channel.`;
+        if (chan !== msg.channel) reply(msg, `${this.container.constants.EMOTES.tick}  ::  Message sent!`);
         return chan.send({
             embeds: [await new MessageEmbed()
                 .setColor(0x40E0D0)
@@ -43,11 +57,15 @@ module.exports = class extends Command {
         });
     }
 
-    async anonymous(msg, [chan = msg.channel, ...msgargs]) {
+    async anonymous(msg, args) {
+        const chan = await args.pick('guildTextChannel').catch(() => msg.channel);
+        let msgargs = await args.restResult('string');
+        if (!msgargs) return reply(msg, `${this.container.constants.EMOTES.xmark}  ::  Please provide the message you want to say.`);
+        msgargs = msgargs.value;
+
         if (!msg.guild || !msg.channel.permissionsFor(this.container.client.user).has('MANAGE_MESSAGES')) throw `${this.container.constants.EMOTES.xmark}  ::  Sorry! I cannot delete messages in this channel.`;
-        msgargs = msgargs.join(this.usageDelim);
-        if (!chan.postable) throw `${this.container.constants.EMOTES.xmark}  ::  Sorry! I cannot send messages in that channel.`;
-        if (chan !== msg.channel) msg.send(`${this.container.constants.EMOTES.tick}  ::  Message sent!`);
+        if (!chan.permissionsFor(this.client.user).has('SEND_MESSAGES')) throw `${this.container.constants.EMOTES.xmark}  ::  Sorry! I cannot send messages in that channel.`;
+        if (chan !== msg.channel) reply(msg, `${this.container.constants.EMOTES.tick}  ::  Message sent!`);
         chan.send({
             embeds: [await new MessageEmbed()
                 .setColor(0x40E0D0)
