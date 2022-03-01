83a636dd8e1bc0c8b881b774aa99729e197c161a
Joshua Permito <dwigoric@insanedevs.com>
2022-03-01 12:32:21 +0800

    fix(modlog): migrate to Sapphire

--- src/commands/Moderation/Server Management/modlog.js
+++ src/commands/Moderation/Server Management/modlog.js
@@ -1,37 +1,44 @@
-const { Command, CommandOptionsRunTypeEnum, RichDisplay, util: { toTitleCase, chunk } } = require('@sapphire/framework');
+const { SubCommandPluginCommand } = require('@sapphire/plugin-subcommands');
+const { CommandOptionsRunTypeEnum } = require('@sapphire/framework');
+const { LazyPaginatedMessage, MessagePrompter } = require('@sapphire/discord.js-utilities');
 const { reply } = require('@sapphire/plugin-editable-commands');
+const { toTitleCase, chunk } = require('@sapphire/utilities');
 const { MessageEmbed, Util: { escapeMarkdown } } = require('discord.js');
 const moment = require('moment-timezone');
 
-module.exports = class extends Command {
+module.exports = class extends SubCommandPluginCommand {
 
-    constructor(...args) {
-        super(...args, {
-            permissionLevel: 6,
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            preconditions: ['ModsOnly'],
             runIn: [CommandOptionsRunTypeEnum.GuildText],
             aliases: ['modlogs', 'log', 'logs'],
-            requiredPermissions: ['EMBED_LINKS', 'MANAGE_MESSAGES'],
+            requiredClientPermissions: ['EMBED_LINKS', 'MANAGE_MESSAGES'],
             description: 'Gives the modlogs for a certain person or the server, or the details of a specific case number.',
-            extendedHelp: [
+            detailedDescription: [
                 'If you want to get the modlogs for the server and not for a user, simple do not provide a user.',
                 'If you want to reset the modlogs (not the channels), use the `reset` subcommand.',
                 'If you want to get the details of a specific case number, simply run `s.modlog <case number here>`.',
                 'To get the modlogs of a certain type, you can use the `--type` flag, e.g. `--type=warn`, `--type=kick`, `ban`, etc.'
             ].join('\n'),
-            usage: '[reset|showcontent] [User:user|CaseNumber:integer]',
-            usageDelim: ' ',
-            subcommands: true
+            options: ['type'],
+            subCommands: ['reset', 'showcontent', { input: 'default', default: true }]
         });
     }
 
-    async messageRun(msg, [user]) {
-        const timezone = msg.author.settings.get('timezone');
-        const { modlogs = [] } = await this.container.databases.default.get('modlogs', msg.guild.id) || {};
+    async default(msg, args) {
+        let user = await args.pickResult('integer');
+        if (!user.success) user = await args.pickResult('user');
+        user = user.success ? user.value : null;
+
+        const { timezone } = this.container.stores.get('gateways').get('userGateway').get(msg.author.id);
+        const { modlogs } = this.container.stores.get('gateways').get('modlogGateway').get(msg.guild.id);
         let list = modlogs.sort((a, b) => parseInt(a.id) - parseInt(b.id));
 
         if (typeof user === 'number') {
             const modlog = list[user - 1];
-            if (!modlog) throw `${this.container.constants.EMOTES.xmark}  ::  Whoops! Seems like Case #${user} doesn't exist on this server... yet.`;
+            if (!modlog) return reply(msg, `${this.container.constants.EMOTES.xmark}  ::  Whoops! Seems like Case #${user} doesn't exist on this server... yet.`);
             const _user = await this.container.client.users.fetch(modlog.user).catch(() => null);
             const moderator = await this.container.client.users.fetch(modlog.moderator).catch(() => null);
             return reply(msg, {
@@ -49,17 +56,19 @@ module.exports = class extends Command {
             });
         }
 
-        if (msg.flagArgs.type && this.container.client.commands.filter(cmd => cmd.category === 'Moderation' && cmd.subCategory === 'Action').keyArray().includes(msg.flagArgs.type)) list = list.filter(ml => ml.type === msg.flagArgs.type); // eslint-disable-line max-len
+        if (args.getOption('type') && this.container.client.commands.filter(cmd => cmd.category === 'Moderation' && cmd.subCategory === 'Action').keyArray().includes(args.getOption('type'))) list = list.filter(ml => ml.type === args.getOption('type')); // eslint-disable-line max-len
         if (user) list = list.filter(ml => ml.user === user.id);
-        if (!list.length) throw `<:blobStop:399433444108533780>  ::  Whoops! It seems that ${user ? user.tag : msg.guild.name} has no record${user ? ' on this server' : ''} yet.`;
-        const message = await msg.channel.send(`${this.container.constants.EMOTES.loading}  ::  Loading moderation logs...`);
-        const display = new RichDisplay(new MessageEmbed()
-            .setColor('RANDOM')
-            .setTitle(`${this.container.constants.EMOTES.blobban} ${list.length} ${msg.flagArgs.type && this.container.client.commands.filter(cmd => cmd.category === 'Moderation' && cmd.subCategory === 'Action').keyArray().includes(msg.flagArgs.type) ? toTitleCase(msg.flagArgs.type) : 'Modlog'}${list.length === 1 ? '' : 's'} for ${user ? `${user.bot ? 'bot' : 'user'} ${user.tag}` : msg.guild.name}`)); // eslint-disable-line max-len
+        if (!list.length) return reply(msg, `${this.container.constants.EMOTES.blobstop}  ::  Whoops! It seems that ${user ? user.tag : msg.guild.name} has no record${user ? ' on this server' : ''} yet.`);
+        const message = await msg.reply(`${this.container.constants.EMOTES.loading}  ::  Loading moderation logs...`);
+        const display = new LazyPaginatedMessage({
+            template: new MessageEmbed()
+                .setColor('RANDOM')
+                .setTitle(`${this.container.constants.EMOTES.blobban} ${list.length} ${args.getOption('type') && this.container.client.commands.filter(cmd => cmd.category === 'Moderation' && cmd.subCategory === 'Action').keyArray().includes(args.getOption('type')) ? toTitleCase(args.getOption('type')) : 'Modlog'}${list.length === 1 ? '' : 's'} for ${user ? `${user.bot ? 'bot' : 'user'} ${user.tag}` : msg.guild.name}`) // eslint-disable-line max-len
+        });
 
         await Promise.all(chunk(list, 5).map(async modlog5 => await Promise.all(modlog5.map(async modlog => {
-            const _user = this.container.client.users.cache.get(modlog.user) || await this.container.client.users.fetch(modlog.user).catch(() => null);
-            const moderator = this.container.client.users.cache.get(modlog.moderator) || await this.container.client.users.fetch(modlog.moderator).catch(() => null);
+            const _user = await this.container.client.users.fetch(modlog.user).catch(() => null);
+            const moderator = await this.container.client.users.fetch(modlog.moderator).catch(() => null);
             return [
                 `__**Case #${modlog.id}**__`,
                 `Type: ${toTitleCase(modlog.type)}`,
@@ -68,35 +77,28 @@ module.exports = class extends Command {
                 `Date: ${moment(modlog.timestamp).tz(timezone).format('dddd, LL | LTS z')} (${moment(modlog.timestamp).fromNow()})`,
                 `Reason: ${modlog.reason ? escapeMarkdown(modlog.reason) : 'Not specified.'}`
             ].join('\n');
-        })))).then(logs => logs.forEach(modlog5 => display.addPage(template => template.setDescription(modlog5.join('\n\n')))));
+        })))).then(logs => logs.forEach(modlog5 => display.addPageEmbed(template => template.setDescription(modlog5.join('\n\n')))));
 
-        return display
-            .setFooterPrefix('Page ')
-            .run(message, { filter: (reaction, author) => author === msg.author });
+        message.edit(`${this.container.constants.EMOTES.tick}  ::  Moderation logs loaded!`);
+        return display.run(message, msg.author);
     }
 
     async showcontent(msg) {
-        const modlogShowContent = msg.guild.settings.get('modlogShowContent');
-        msg.guild.settings.update('modlogShowContent', !modlogShowContent);
-        return msg.send(`${this.container.constants.EMOTES.tick}  ::  Content is now ${modlogShowContent ? 'not ' : ''}modlogged.`);
+        const { modlogShowContent } = this.container.stores.get('gateways').get('guildGateway').get(msg.guild.id);
+        this.container.stores.get('gateways').get('guildGateway').update(msg.guild.id, 'modlogShowContent', !modlogShowContent);
+        return reply(msg, `${this.container.constants.EMOTES.tick}  ::  Content is now ${modlogShowContent ? 'not ' : ''}modlogged.`);
     }
 
     async reset(msg) {
-        let prompt;
-        do {
-            prompt = await msg.prompt('⚠ Are you sure you want to reset **all** modlogs? Respond with `yes` or `no`.').catch(() => ({ content: 'no' }));
-        } while (!['yes', 'no', null].includes(prompt.content));
-        if (prompt.content === 'yes') {
-            await this.container.databases.default.update('modlogs', msg.guild.id, { modlogs: [] });
-            return msg.send(`${this.container.constants.EMOTES.tick}  ::  Successfully reset the modlogs of **${msg.guild.name}**.`);
-        } else {
-            return msg.send(`${this.container.constants.EMOTES.tick}  ::  Alright! You don't want to reset your modlogs.`);
-        }
-    }
+        const handler = new MessagePrompter('⚠ Are you sure you want to reset **all** modlogs?', 'confirm');
+        const prompt = await handler.run(msg.channel, msg.author).catch(() => null);
 
-    async init() {
-        const defProvider = this.container.databases.default;
-        if (!await defProvider.hasTable('modlogs')) defProvider.createTable('modlogs');
+        if (prompt) {
+            await this.container.stores.get('gateways').get('modlogGateway').reset(msg.guild.id, 'modlogs');
+            return reply(msg, `${this.container.constants.EMOTES.tick}  ::  Successfully reset the modlogs of **${msg.guild.name}**.`);
+        } else {
+            return reply(msg, `${this.container.constants.EMOTES.tick}  ::  Alright! You don't want to reset your modlogs.`);
+        }
     }
 
 };
