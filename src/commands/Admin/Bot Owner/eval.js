282398c697e9c347c1dd2455d130a91e50a57f3b
Joshua Permito <dwigoric@insanedevs.com>
2022-03-10 08:19:49 +0800

    fix(eval): export as file correctly

--- src/commands/Admin/Bot Owner/eval.js
+++ src/commands/Admin/Bot Owner/eval.js
@@ -58,7 +58,7 @@ module.exports = class extends Command {
     async handleMessage(msg, options, { success, result, time, footer, language }) {
         switch (options.sendAs) {
             case 'file': {
-                if (msg.channel.attachable) {
+                if (msg.channel.permissionsFor(this.container.client.user).has(['SEND_MESSAGES', 'ATTACH_FILES'])) {
                     return reply(msg, {
                         files: [{ name: 'output.txt', attachment: Buffer.from(result) }],
                         content: `Sent the result as a file.\n**Type**:${footer}\n${time}`
@@ -94,7 +94,7 @@ module.exports = class extends Command {
 
     async getTypeOutput(msg, options) {
         const _options = ['log'];
-        if (msg.channel.attachable) _options.push('file');
+        if (msg.channel.permissionsFor(this.container.client.user).has(['SEND_FILES', 'ATTACH_FILES'])) _options.push('file');
         if (!options.hastebinUnavailable) _options.push('hastebin');
         const handler = new MessagePrompter(`Choose one of the following options: ${_options.join(', ')}`, 'message');
         let _choice;
