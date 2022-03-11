b9c8fe7adee5417b4cd83ece7b94b6c050c57813
Joshua Permito <dwigoric@insanedevs.com>
2022-03-11 18:17:58 +0800

    fix(DJOnly): use guild gateway

--- src/preconditions/DJOnly.js
+++ src/preconditions/DJOnly.js
@@ -5,9 +5,9 @@ module.exports = class DJOnlyPrecondition extends Precondition {
     async run(msg) {
         if (!msg.member) return this.ok();
         if (msg.member.permissions.has('ADMINISTRATOR')) return this.ok();
-        const musicGateway = this.container.stores.get('gateways').get('musicGateway').get(msg.guild.id);
-        if (!musicGateway.music.dj.length) return this.ok();
-        if (musicGateway.music.dj.some(role => Array.from(msg.member.roles.cache.keys()).includes(role))) return this.ok();
+        const guildGateway = this.container.stores.get('gateways').get('guildGateway').get(msg.guild.id);
+        if (!guildGateway.music.dj.length) return this.ok();
+        if (guildGateway.music.dj.some(role => Array.from(msg.member.roles.cache.keys()).includes(role))) return this.ok();
         return this.error();
     }
 
