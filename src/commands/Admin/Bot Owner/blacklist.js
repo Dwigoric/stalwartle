4be95ca14b0a812c198543ff95193c4c4efb2206
Joshua Permito <dwigoric@insanedevs.com>
2022-03-12 09:59:54 +0800

    chore: transfer and migrate core Admin commands

---
+++ src/commands/Admin/Bot Owner/blacklist.js
@@ -0,0 +1,49 @@
+const { Command } = require('@sapphire/framework');
+const { reply } = require('@sapphire/plugin-editable-commands');
+const { codeBlock } = require('@sapphire/utilities');
+const { User } = require('discord.js');
+
+module.exports = class extends Command {
+
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            preconditions: ['OwnersOnly'],
+            description: 'Blacklists or un-blacklists users and guilds from the bot.'
+        });
+
+        this.terms = ['usersAdded', 'usersRemoved', 'guildsAdded', 'guildsRemoved'];
+    }
+
+    async messageRun(message, args) {
+        let usersAndGuilds = await args.pickResult('user');
+        if (!usersAndGuilds.success) usersAndGuilds = await args.restResult('string');
+        if (!usersAndGuilds.success) return reply(message, `${this.container.constants.EMOTES.xmark}  ::  Please give the user or guild ID to blacklist.`);
+        usersAndGuilds = [usersAndGuilds.value];
+
+        const changes = [[], [], [], []];
+        const { userBlacklist, guildBlacklist } = this.container.client.settings;
+
+        for (const userOrGuild of new Set(usersAndGuilds)) {
+            const type = userOrGuild instanceof User ? 'user' : 'guild';
+            const usedArray = type === 'user' ? userBlacklist : guildBlacklist;
+            if (this.container.client.settings[`${type}Blacklist`].includes(userOrGuild.id || userOrGuild)) {
+                changes[this.terms.indexOf(`${type}sRemoved`)].push(userOrGuild.name || userOrGuild.username || userOrGuild);
+                usedArray.splice(usedArray.indexOf(userOrGuild.id || userOrGuild), 1);
+            } else {
+                changes[this.terms.indexOf(`${type}sAdded`)].push(userOrGuild.name || userOrGuild.username || userOrGuild);
+                usedArray.push(userOrGuild.id || userOrGuild);
+            }
+        }
+
+        await this.container.stores.get('gateways').get('clientGateway').update(this.container.client.user.id, { userBlacklist, guildBlacklist });
+
+        return reply(message, ((usersAdded, usersRemoved, guildsAdded, guildsRemoved) => [
+            usersAdded.length ? `**Users Added**\n${codeBlock('', usersAdded.join(', '))}` : '',
+            usersRemoved.length ? `**Users Removed**\n${codeBlock('', usersRemoved.join(', '))}` : '',
+            guildsAdded.length ? `**Guilds Added**\n${codeBlock('', guildsAdded.join(', '))}` : '',
+            guildsRemoved.length ? `**Guilds Removed**\n${codeBlock('', guildsRemoved.join(', '))}` : ''
+        ].filter(val => val !== '').join('\n'))(...changes));
+    }
+
+};
---
+++ src/commands/Admin/Bot Owner/enable.js
@@ -0,0 +1,27 @@
+const { Command } = require('@sapphire/framework');
+const { reply } = require('@sapphire/plugin-editable-commands');
+
+module.exports = class extends Command {
+
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            preconditions: ['OwnersOnly'],
+            description: 'Re-enables or temporarily enables a piece. Default state restored on reboot.'
+        });
+    }
+
+    async messageRun(message, args) {
+        const piece = await args.pick('piece').catch(() => null);
+        if (piece === null) return reply(message, `${this.container.constants.EMOTES.xmark}  ::  Please supply the name of the piece you want to enable.`);
+
+        piece.enabled = true;
+        if (this.container.client.shard) {
+            await this.container.client.shard.broadcastEval(`
+                if (String(this.options.shards) !== '${this.container.client.options.shards}') this.container.stores.get('${piece.store.name}').get('${piece.name}').enabled = true;
+            `);
+        }
+        return reply(message, `${this.container.constants.EMOTES.tick}  ::  Successfully enabled \`${piece.name}\` to ${piece.store.name}.`);
+    }
+
+};
