8137f97023c0768a24712be1e1ca13e344a72b1d
Joshua Permito <dwigoric@insanedevs.com>
2022-03-20 22:17:51 +0800

    fix(Autopause): use erela.js logic

--- src/listeners/Autopause.js
+++ src/listeners/Autopause.js
@@ -10,30 +10,33 @@ module.exports = class extends Listener {
 
     async run(oldState, newState) {
         if (!this.container.erela) return null;
-        if (!this.container.erela.players.get(newState.guild.id)) return null;
-        if (!newState.guild.me.voice.channel) return this.container.erela.destroy(newState.guild.id);
-        if (oldState.channel && newState.channel && (oldState.channel.id === newState.channel.id || ![oldState.channel.id, newState.channel.id].includes(newState.guild.me.voice.channel))) return null;
+
+        const player = this.container.erela.players.get(newState.guild.id);
+        if (!player) return null;
+        if (!newState.guild.me.voice.channelId) return player.destroy();
+        if (oldState.channelId && newState.channelId && (oldState.channelId === newState.channelId || ![oldState.channelId, newState.channelId].includes(newState.guild.me.voice.channelId))) return null;
 
         const channelMembers = newState.guild.me.voice.channel.members.filter(mb => !mb.user.bot);
-        if (this.container.erela.players.has(newState.guild.id) && !this.container.erela.players.get(newState.guild.id).playing && !channelMembers.size) {
-            clearTimeout(this.container.client.commands.get('play').timeouts.get(newState.guild.id));
-            this.container.client.commands.get('play').timeouts.delete(newState.guild.id);
-            return this.container.erela.players.get(newState.guild.id).destroy();
+        if (player && !player.playing && !channelMembers.size) {
+            clearTimeout(this.container.stores.get('commands').get('play').timeouts.get(newState.guild.id));
+            this.container.stores.get('commands').get('play').timeouts.delete(newState.guild.id);
+            return player.destroy();
         }
-        if (newState.guild.me.voice.channel && channelMembers.size && this.#autopaused.has(newState.guild.id)) {
+        if (newState.guild.me.voice.channelId && channelMembers.size && this.#autopaused.has(newState.guild.id)) {
             this.#autopaused.delete(newState.guild.id);
-            return this.container.erela.players.get(newState.guild.id).pause(false);
+            return player.pause(false);
         }
         if (channelMembers.size) return null;
-        const { queue } = await this.container.stores.get('gateways').get('musicGateway').get(newState.guild.id);
-        if (!queue[0].info.isStream) {
+
+        const { queue } = player;
+        if (!queue.current.isStream) {
             this.#autopaused.add(newState.guild.id);
-            this.container.erela.players.get(newState.guild.id).pause(true);
+            player.pause(true);
         }
         if (this.container.stores.get('gateways').get('guildGateway').get(newState.guild.id).donation >= 10) return null;
         return this.container.client.setTimeout(guild => {
             if (guild.me.voice.channel && guild.me.voice.channel.members.filter(mb => !mb.user.bot).size) return null;
-            this.container.erela.players.get(guild.id).destroy();
+            if (player) player.destroy();
             return null;
         }, 30000, newState.guild);
     }
