602e69e9c639ea5a96d3591f418989e2ba095154
Joshua Permito <dwigoric@insanedevs.com>
2022-04-05 17:32:28 +0800

    misc(reminder): show reminder user in channels

--- src/scheduled-tasks/Reminder.js
+++ src/scheduled-tasks/Reminder.js
@@ -6,7 +6,9 @@ module.exports = class extends ScheduledTask {
         const _channel = this.container.client.channels.cache.get(channel);
         const _user = await this.container.client.users.fetch(user);
         const _text = text ? `me to remind you: ${text}` : 'to be reminded!';
-        const reminder = forceChannel ? `Hey there, people of ${_channel}! You wanted ${_text}` : `Hey there, ${_user}! You wanted ${_text}`;
+        const reminder = forceChannel ?
+            `Hey there, people of ${_channel}! **${_user.tag}** wanted ${_text}` :
+            `Hey there, ${_user}! You wanted ${_text}`;
         if (!_channel) return null;
         if (forceChannel) return _channel.send(reminder).catch(() => null);
         else return _user.send(reminder).catch(() => _channel.send(reminder).catch(() => null));
