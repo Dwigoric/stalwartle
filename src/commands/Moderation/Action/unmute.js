424bc9e100e84946083ea66fd16916d2511aad61
Joshua Permito <dwigoric@insanedevs.com>
2022-07-23 11:01:29 +0800

    misc(unmute): remnant of the past

--- src/commands/Moderation/Action/unmute.js
+++ src/commands/Moderation/Action/unmute.js
@@ -23,9 +23,6 @@ module.exports = class extends Command {
         if (member.user.id === msg.author.id) return reply(msg, 'Look... how are you able to use this command if you were already unmuted?');
         if (member.user.id === this.container.client.user.id) return reply(msg, '*Unmutes self*. Oh, I would not have been able to respond if I were in the first place!');
 
-        const task = (await this.container.tasks.list({})).filter(job => job.data.task === 'Unmute' && job.data.payload.user === member.id)[0];
-        if (task) this.container.tasks.delete(task.id);
-
         reply(msg, `${this.container.constants.EMOTES.tick}  ::  **${member.user.tag}** (\`${member.id}\`) has been unmuted. ${reason ? `**Reason**: ${reason}` : ''}`);
         return this.container.client.emit('modlogAction', msg, member.user, reason);
     }
