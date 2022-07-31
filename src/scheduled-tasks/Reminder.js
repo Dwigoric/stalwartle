2ba28e4d43c3f45fe4eb5f984e9cfe9fff7ec296
Joshua Permito <dwigoric@insanedevs.com>
2022-07-31 10:07:07 +0800

    fix(Reminder): skip everyone mention setting check on DMs

--- src/scheduled-tasks/Reminder.js
+++ src/scheduled-tasks/Reminder.js
@@ -6,15 +6,16 @@ module.exports = class extends ScheduledTask {
         const _channel = this.container.client.channels.cache.get(channel);
         const _user = await this.container.client.users.fetch(user);
         const _text = text ? `remind you: ${text}` : 'give this reminder. (No details provided)';
-        const reminder = forceChannel ?
+        const reminder = forceChannel && _channel.type !== 'DM' ?
             `Hey there, people of ${_channel}! **${_user.tag}** wanted me to ${_text}` :
             `Hey there, ${_user}! You wanted me to ${_text}`;
         const parseMentions = ['roles', 'users'];
 
         if (!_channel) return null;
-        if (this.container.stores.get('gateways').get('guildGateway').get(_channel.guild.id).allowRemindEveryone) parseMentions.push('everyone');
+        if (_channel.guild && this.container.stores.get('gateways').get('guildGateway').get(_channel.guild.id).allowRemindEveryone) parseMentions.push('everyone');
 
-        return forceChannel ? _channel.send({ content: reminder, allowedMentions: { parse: parseMentions } }).catch(() => null) : _user.send(reminder).catch(() => _channel.send(reminder).catch(() => null));
+        return _channel.type === 'DM' ? _user.send({ content: reminder, allowedMentions: { parse: parseMentions } }).catch(() => null) :
+            forceChannel ? _channel.send({ content: reminder, allowedMentions: { parse: parseMentions } }).catch(() => null) : _user.send(reminder).catch(() => _channel.send(reminder).catch(() => null));
     }
 
 };
