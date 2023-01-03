4fada5ca7a1e94a4ecb0d6a0545ac54091a80a87
Joshua Permito <dwigoric@insanedevs.com>
2023-01-03 21:40:30 +0800

    fix: use explicit DM channel type

--- src/lib/util/reply.js
+++ src/lib/util/reply.js
@@ -1,9 +1,8 @@
 const { send, reply } = require('@sapphire/plugin-editable-commands');
 const { Permissions } = require('discord.js');
-const { ChannelTypes } = require('discord.js/typings/enums');
 
 module.exports = (message, options) => {
-    const canReadHistory = message.channel.type === ChannelTypes.DM || message.channel.permissionsFor(message.guild.me).has(Permissions.FLAGS.READ_MESSAGE_HISTORY);
+    const canReadHistory = message.channel.type === 'DM' || message.channel.permissionsFor(message.guild.me).has(Permissions.FLAGS.READ_MESSAGE_HISTORY);
     return (canReadHistory ? reply : send)(message, options)
         .catch(err => canReadHistory ?
             send(message, options) :
